eec33074bdac2b2a882ce17699fc5b3df463b322
Claude.CHOISNET@gmail.com <Claude.CHOISNET@gmail.com@bc589049-105d-1923-a3ce-6fcb24364ed1>
2010-10-03 21:34:12 +0000

    git-svn-id: https://cchlib.googlecode.com/svn/trunk@4 bc589049-105d-1923-a3ce-6fcb24364ed1

---
+++ CHCLibCore/.build-junit.cmd
@@ -0,0 +1,13 @@
+@Echo OFF
+@Rem
+@Rem -----------------------------------------------------------------------
+@Rem Nom           : .build-junit.cmd
+@Rem Description   :
+@Rem Encodage      : DOS
+@Rem
+@Rem 1.00 2010.08.24  CC - Version initiale
+@Rem -----------------------------------------------------------------------
+@Rem
+@Rem
+@Rem
+@Call "%~pd0.build.cmd" junit
