e2670f481460568224135e567e3f64de2d9c76de
Claude.CHOISNET@gmail.com <Claude.CHOISNET@gmail.com@bc589049-105d-1923-a3ce-6fcb24364ed1>
2011-02-18 21:07:37 +0000

    CHCLibCore - 03a.34   Add: cx.ath.choisnet.util.LinkedHashMapList
    git-svn-id: https://cchlib.googlecode.com/svn/trunk@93 bc589049-105d-1923-a3ce-6fcb24364ed1

--- CHCLibCore/src/cx/ath/choisnet/util/LinkedHashMapList.java
+++ CHCLibCore/src/cx/ath/choisnet/util/LinkedHashMapList.java
@@ -2,46 +2,35 @@ package cx.ath.choisnet.util;
 
 import java.io.Serializable;
 import java.util.AbstractCollection;
+import java.util.ArrayList;
 import java.util.Collection;
-import java.util.HashMap;
-import java.util.HashSet;
 import java.util.Iterator;
+import java.util.LinkedHashMap;
+import java.util.List;
 import java.util.Map;
 import java.util.Set;
 import cx.ath.choisnet.util.iterator.CascadingIterator;
 
 /**
- * HashMapSet provide an easy and efficient way
+ * LinkedHashMapList provide an easy and efficient way
  * to store keys-values when you need to have
  * more than one value for an unique key.
  * <br/>
  * <br/>
  * Concrete examples:
  * <p>
- * - Store File objects and grouping files with same
- *   size : HashMapSet&lt;Long,File&gt;
- *   <br/>
- *   Where keys are length of files, and values are
- *   File object. 
- * </p>
- * <p>
- * - Store File objects and grouping files with
- *   content size : HashMapSet&lt;<i>any_hash_code</i>,File&gt;
- *   <br/>
- *   Where keys are an hash code based on content (MD5,...), 
- *   and values are File object. 
+ * - Store File CustomObject and grouping then by date
  * </p>
  * <p>
  * <b>Starting with this class:</b>
  * <pre>
- *  HashMapSet&lt;Long,File&gt; hashMapSet = HashMapSet&lt;Long,File&gt;()
+ *  LinkedHashMapList&lt;Date,CustomObject&gt; hashMapSet = HashMapSet&lt;Date,CustomObject&gt;()
  *  
  *  for(...) {
- *    File file = ...
- *    hashMapSet.add( new Long(file.length), file);
+ *    CustomObject myObject = ...
+ *
+ *    hashMapSet.add( myObject.getDate(), myObject);
  *    }
- *    
- *  
  * </pre>
  * </p>
  * 
@@ -49,37 +38,36 @@ import cx.ath.choisnet.util.iterator.CascadingIterator;
  * @param <K> the type of keys maintained by this map
  * @param <V> the type of mapped values
  */
-public class HashMapSet<K,V> 
-    extends HashMap<K,Set<V>>
+public class LinkedHashMapList<K,V> 
+    extends LinkedHashMap<K,List<V>>
         implements  Iterable<V>,
                     Serializable
-
 {
     private static final long serialVersionUID = 1L;
     
     /**
-     * Constructs an empty HashMapSet with the default 
+     * Constructs an empty LinkedHashMapList with the default 
      * initial capacity (16) and the default load
      * factor (0.75). 
      */
-    public HashMapSet()
+    public LinkedHashMapList()
     {
         super();
     }
 
     /**
-     * Constructs an empty HashMapSet with the specified
+     * Constructs an empty LinkedHashMapList with the specified
      * initial capacity and the default load factor (0.75). 
      * @param initialCapacity the initial capacity. 
      */
-    public HashMapSet(int initialCapacity)
+    public LinkedHashMapList(int initialCapacity)
     {
         super(initialCapacity);
     }
 
     /**
-     * Constructs a new HashMapSet with the same mappings
-     * as the specified Map. The HashMapSet is created with
+     * Constructs a new LinkedHashMapList with the same mappings
+     * as the specified Map. The LinkedHashMapList is created with
      * default load factor (0.75) and an initial
      * capacity sufficient to hold the mappings in the
      * specified Map.
@@ -87,37 +75,37 @@ public class HashMapSet<K,V>
      * @param m the map whose mappings are to be 
      *          placed in this map 
      */
-    public HashMapSet( Map<? extends K,? extends Set<V>> m )
+    public LinkedHashMapList( Map<? extends K,? extends List<V>> m )
     {
         super(m);
     }
 
     /**
-     * Constructs an empty HashMapSet with the specified 
+     * Constructs an empty LinkedHashMapList with the specified 
      * initial capacity and load factor. 
      * 
      * @param initialCapacity the initial capacity
      * @param loadFactor the load factor 
      */
-    public HashMapSet( int initialCapacity, float loadFactor )
+    public LinkedHashMapList( int initialCapacity, float loadFactor )
     {
         super(initialCapacity,loadFactor);
     }
 
     /**
-     * Removes all of the mappings from this HashMapSet,
+     * Removes all of the mappings from this LinkedHashMapList,
      * but also perform a {@link Set#clear()} on each
      * set of values.
      * <br>
-     * The HashMapSet will be empty after this call returns. 
+     * The LinkedHashMapList will be empty after this call returns. 
      * 
      * @see #clear()
      */
     public void deepClear()
     {
-        for(Set<V> s:super.values()) {
+        for( List<V> s:super.values() ) {
             s.clear();
-        }
+            }
         
         super.clear();
     }
@@ -138,40 +126,13 @@ public class HashMapSet<K,V>
     {
         int size = 0;
         
-        for(Set<? extends V> s:super.values()) {
+        for(List<? extends V> s:super.values()) {
             size += s.size();
-        }
+            }
         
         return size;
     }
 
-//    /** CAN'T OVERWRITE containsValue() -> buggy
-//     * Returns true if this map maps
-//     * one or more keys to the specified value.
-//     * <p>
-//     * If value is a Set, looking for a Set&lt;V&gt;
-//     * in HashMapSet.
-//     * <br/>
-//     * If value is not a Set, looking throws all
-//     * Set&lt;V&gt; to find at least a matching value.
-//     * 
-//     * @return true if this map maps contains the
-//     *         specified value
-//     */
-//    @Override // Map
-//    public boolean containsValue( Object value )
-//    {
-//        if( value instanceof Set ) {
-//            return super.containsValue( value );
-//        }
-//        for(Set<? extends V> s:super.values()) {
-//            if( s.contains( value )) {
-//                return true;
-//            }
-//        }
-//        return false;
-//    }
-    
     /**
      * Not supported
      * 
@@ -184,11 +145,11 @@ public class HashMapSet<K,V>
     }
 
     /**
-     * Add an couple of key value in this HashMapSet.
+     * Add an couple of key value in this LinkedHashMapList.
      * <p>
-     * If key already exist in this HashMapSet, add value
+     * If key already exist in this LinkedHashMapList, add value
      * to corresponding set. If key does not exist create
-     * an new HashSet initialized with this value, and 
+     * an new {@link ArrayList} initialized with this value, and 
      * Associates this set with the specified key in this 
      * HashMapSet.
      * </p>
@@ -200,22 +161,23 @@ public class HashMapSet<K,V>
      */
     public boolean add( K key, V value )
     {
-        Set<V> s = get(key);
+        List<V> s = get(key);
 
         if( s == null ) {
-            s = new HashSet<V>();
+            s = new ArrayList<V>();
             
             super.put(key,s);
-        }
+            }
+
         return s.add( value );
     }
     
     /**
-     * Add all couples of key value in this HashMapSet.
+     * Add all couples of key value in this LinkedHashMapList.
      * Use {@link #add(Object, Object)} for each entries.
      * 
-     * @param m map to add to current HashMapSet
-     * @return number of values add in HashMapSet.
+     * @param m map to add to current LinkedHashMapList
+     * @return number of values add in LinkedHashMapList.
      * @see #add(Object, Object) add(Object, Object) for more details
      */
     public int addAll(Map<K,V> m)
@@ -225,64 +187,65 @@ public class HashMapSet<K,V>
         for(Map.Entry<K,V> e:m.entrySet()) {
            if( add( e.getKey(), e.getValue() ) ) {
                r++;
-           }
-        }
+               }
+            }
        
        return r;
     }
     
     /**
-     * Add all values with same key in this HashMapSet.
+     * Add all values to the same key in this LinkedHashMapList.
      * <p>
      * Tips:<br/>
-     * If you want to replace a Set&lt;V&gt; for a key, use {@link #put(Object, Object)}
+     * If you want to replace a List&lt;V&gt; for a key, use {@link #put(Object, Object)}
      * </p>
      * @param key       key to use for all values
      * @param values    values to add
      *
      * @return number of values add in HashMapSet.
      */
-    public int addAll(K key, Collection<V> values)
+    public int addAll(K key, Collection<? extends V> values)
     {
         int     r = 0;
-        Set<V>  s = get(key);
+        List<V> s = get(key);
 
         if( s == null ) {
-            s = new HashSet<V>();
+            s = new ArrayList<V>();
 
             super.put(key,s);
-        }
+            }
 
         for(V v:values) {
            if( s.add( v ) ) {
                r++;
-           }
-        }
+               }
+            }
 
        return r;
     }
 
     /**
-     * Removes the specified key-value from this HashMapSet
+     * Removes the specified key-value from this LinkedHashMapList
      * if it is present.
      * 
      * @param key  key with which the specified value is to be associated
      * @param value value to be associated with the specified key 
      * 
-     * @return if this set HashMapSet the specified key-value 
+     * @return if this set LinkedHashMapList the specified key-value 
      */
     public boolean remove(K key, V value)
     {
-        Set<V> s = super.get( key );
+        List<V> s = super.get( key );
         
         if( s != null ) {
             return s.remove( value );
-        }
+            }
+        
         return false;
     }
     
     /**
-     * Returns true if this HashMapSet contains the specified
+     * Returns true if this LinkedHashMapList contains the specified
      * element. More formally, returns true if and only if
      * at least one Set contains at least one element e such
      * that (value==null ? e==null : o.equals(e)). 
@@ -290,25 +253,25 @@ public class HashMapSet<K,V>
      * @param value element whose presence in this MapOfSet is 
      *              to be tested 
      *              
-     * @return true if this HashMapSet contains the specified element 
+     * @return true if this LinkedHashMapList contains the specified element 
      */
     public boolean contains( V value )
     {// from Collection<V>
-        for(Set<? extends V> s:super.values()) {
+        for(List<? extends V> s:super.values()) {
             if( s.contains( value )) {
                 return true;
+                }
             }
-        }
         return false;
     }
 
     /**
-     * Returns true if this HashMapSet contains all 
+     * Returns true if this LinkedHashMapList contains all 
      * of the elements in the specified collection.
      * 
      * @param c collection to be checked for 
-     *          containment in this HashMapSet
-     * @return true if this HashMapSet contains all 
+     *          containment in this LinkedHashMapList
+     * @return true if this LinkedHashMapList contains all 
      *         of the elements in the specified collection 
      */
     public boolean containsAll( Collection<? extends V> c )
@@ -316,20 +279,20 @@ public class HashMapSet<K,V>
         for(V v:c) {
             if( !contains(v) ) {
                 return false;
+                }
             }
-        }
         return true;
     }
 
     /**
      * Returns an iterator over the values in this
-     * HashMapSet.
+     * LinkedHashMapList.
      * <p>
      * If you use {@link Iterator#remove()} you must
-     * consider to {@link #purge()} HashMapSet.
+     * consider to {@link #purge()} LinkedHashMapList.
      * </p>
      * @return an Iterator over the values in 
-     *         this HashMapSet
+     *         this LinkedHashMapList
      */
     @Override //Iterable
     public Iterator<V> iterator()
@@ -348,20 +311,20 @@ public class HashMapSet<K,V>
      * not contains more than 1 value.
      * </p>
      * @param minSetSize minimum size for Sets to be
-     *        keep in HashMapSet
+     *        keep in LinkedHashMapList
      */
     public void purge(int minSetSize)
     {
-        Iterator<Map.Entry<K, Set<V>>> iter = super.entrySet().iterator();
+        Iterator<Map.Entry<K,List<V>>> iter = super.entrySet().iterator();
         
         while(iter.hasNext()) {
-           Map.Entry<K, Set<V>> e = iter.next();
-           Set<V>               s = e.getValue();
+           Map.Entry<K,List<V>> e = iter.next();
+           List<V>              s = e.getValue();
            
            if( (s==null) || (s.size()<minSetSize) ) {
                iter.remove();
-           }
-        }
+               }
+            }
     }
     
     /**
@@ -378,7 +341,7 @@ public class HashMapSet<K,V>
     
     /**
      * Returns an <b>unmodifiable</b> Collection view
-     * of V according to HashMapSet.
+     * of V according to LinkedHashMapList.
      * 
      * @return an unmodifiable Collection view
      *         of V
@@ -403,7 +366,7 @@ public class HashMapSet<K,V>
 
     /**
      * Get all values on Iterable object, compute
-     * their keys add add(key,values) in this HashMapSet.
+     * their keys add add(key,values) in this LinkedHashMapList.
      * <p>
      * <b>Example of use:</b><br/>
      * This is probably most efficient way to use
@@ -411,7 +374,7 @@ public class HashMapSet<K,V>
      * <pre>
      * <u>final</u> Iterable&lt;TVALUE&gt; iterableFinal = <i>any_iterable_object_like_collections</i>; 
      * 
-     * hashMapSet.addAll(
+     * linkedHashMapList.addAll(
      *       <u>new ComputeKeyIterable&lt;TKEY,TVALUE&gt;()</u>
      *       {
      *           {@code @Override}
@@ -432,15 +395,15 @@ public class HashMapSet<K,V>
      *                 compute key for each value.
      * @see ComputeKeyInterface
      */
-    public void addAll( ComputeKeyIterable<K,V> iterable )
+    public void addAll( HashMapSet.ComputeKeyIterable<K,V> iterable )
     {
         Iterator<V> i = iterable.iterator();
         
-        while(i.hasNext()) {
+        while( i.hasNext() ) {
             V v = i.next();
             K k = iterable.computeKey( v );
             add(k,v);
-        }
+            }
     }
     
     /**
@@ -449,23 +412,23 @@ public class HashMapSet<K,V>
      * @param iterator iterator used to get values and compute
      *        theirs keys.
      */
-    public void addAll( ComputeKeyIterator<K,V> iterator )
+    public void addAll( HashMapSet.ComputeKeyIterator<K,V> iterator )
     {
         while(iterator.hasNext()) {
             V v = iterator.next();
             K k = iterator.computeKey( v );
             add(k,v);
-        }
+            }
     }
 
-    /**
+    /* *
      * Compute key from value
      * 
      * @author Claude CHOISNET
      * @param <K> the type of keys computed from current value
      * @param <V> the type of values
-     * @see HashMapSet.ComputeKeyIterable
-     */
+     * @see LinkedHashMapList.ComputeKeyIterable
+     * /
     public interface ComputeKeyInterface<K,V>
     {
         /**
@@ -473,55 +436,55 @@ public class HashMapSet<K,V>
          * 
          * @param value
          * @return key for current value.
-         */
+         * /
         public K computeKey(V value);
-    }
+    }*/
     
-    /**
+    /* *
      * Define an Iterable object able to compute key
      * from values.
      * <p>
      * This is probably the most efficient way to
-     * use {@link HashMapSet.ComputeKeyInterface}
+     * use {@link LinkedHashMapList.ComputeKeyInterface}
      * </p>
      * 
      * @author Claude CHOISNET
      * @param <K> the type of keys computed from current value
      * @param <V> the type of values
-     * @see HashMapSet.ComputeKeyInterface
-     * @see HashMapSet.ComputeKeyIterator
-     */
+     * @see LinkedHashMapList.ComputeKeyInterface
+     * @see LinkedHashMapList.ComputeKeyIterator
+     * /
     public interface ComputeKeyIterable<K,V> 
         extends Iterable<V>, ComputeKeyInterface<K,V>
     {
-    }
+    }*/
     
-    /**
+    /* *
      * Define an Iterator object able to compute key
      * from values.
      * 
      * @author Claude CHOISNET
      * @param <K> the type of keys computed from current value
      * @param <V> the type of values
-     * @see HashMapSet.ComputeKeyInterface
-     * @see HashMapSet.ComputeKeyIterable
-     */
+     * @see LinkedHashMapList.ComputeKeyInterface
+     * @see LinkedHashMapList.ComputeKeyIterable
+     * /
     public interface ComputeKeyIterator<K,V>
         extends Iterator<V>, ComputeKeyInterface<K,V>
     {
         
-    }
+    }*/
     
-    /**
+    /* *
      * Default Iterator object able to compute key
      * from values.
      * 
      * @author Claude CHOISNET
      * @param <K> the type of keys computed from current value
      * @param <V> the type of values
-     * @see HashMapSet.ComputeKeyInterface
-     * @see HashMapSet.ComputeKeyIterable
-     */
+     * @see LinkedHashMapList.ComputeKeyInterface
+     * @see LinkedHashMapList.ComputeKeyIterable
+     * /
     public static abstract class AbstractComputeKeyIterator<K,V>
         implements ComputeKeyIterator<K,V>
     {
@@ -550,5 +513,6 @@ public class HashMapSet<K,V>
         {
             iterator.remove();
         }
-    }
+    }*/
+
 }
