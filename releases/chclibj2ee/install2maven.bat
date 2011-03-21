b530df153d9cb648ff3ede23eef12fcbf7c45b10
Claude.CHOISNET@gmail.com <Claude.CHOISNET@gmail.com@bc589049-105d-1923-a3ce-6fcb24364ed1>
2011-03-21 06:31:24 +0000

    CHCLibJ2EE - 03a.10
    git-svn-id: https://cchlib.googlecode.com/svn/trunk@126 bc589049-105d-1923-a3ce-6fcb24364ed1

--- releases/chclibj2ee/install2maven.bat
+++ releases/chclibj2ee/install2maven.bat
@@ -1,4 +1,4 @@
-Set MYVER=03a.09.36
+Set MYVER=03a.10.39
 
 CALL mvn install:install-file -Dfile=CHCLibJ2EE.%MYVER%.jar -DgroupId=com.googlecode.chclib -DartifactId=chclibj2ee -Dversion=%MYVER% -Dpackaging=jar
 
