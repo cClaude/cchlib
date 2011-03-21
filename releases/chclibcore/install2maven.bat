82f9a5e8ada664ceb326d6344f49172a9b54ccc4
Claude.CHOISNET@gmail.com <Claude.CHOISNET@gmail.com@bc589049-105d-1923-a3ce-6fcb24364ed1>
2011-03-21 06:50:31 +0000

    CHCLibCore - 03a.37
    git-svn-id: https://cchlib.googlecode.com/svn/trunk@128 bc589049-105d-1923-a3ce-6fcb24364ed1

--- releases/chclibcore/install2maven.bat
+++ releases/chclibcore/install2maven.bat
@@ -1,4 +1,4 @@
-Set MYVER=03a.37.6
+Set MYVER=03a.37.9
 
 CALL mvn install:install-file -Dfile=CHCLibCore.%MYVER%.jar -DgroupId=com.googlecode.chclib -DartifactId=chclibcore -Dversion=%MYVER% -Dpackaging=jar
 
