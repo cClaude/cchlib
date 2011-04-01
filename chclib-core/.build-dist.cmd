465b7056bc26ce53568b2a13e6eead234a264b3a
Claude.CHOISNET@gmail.com <Claude.CHOISNET@gmail.com@bc589049-105d-1923-a3ce-6fcb24364ed1>
2011-04-01 15:47:10 +0000

    CHCLib (maven migration step 2)
    git-svn-id: https://cchlib.googlecode.com/svn/trunk@145 bc589049-105d-1923-a3ce-6fcb24364ed1

---
+++ chclib-core/.build-dist.cmd
@@ -0,0 +1,15 @@
+@Echo OFF
+@Rem
+@Rem -----------------------------------------------------------------------
+@Rem Nom           : .build-jar.cmd
+@Rem Description   :
+@Rem Encodage      : DOS
+@Rem
+@Rem 1.00 2010.08.20  CC - Version initiale
+@Rem -----------------------------------------------------------------------
+@Rem
+@Rem
+@Rem
+@Call "%~pd0.build.cmd" dist
+
+
