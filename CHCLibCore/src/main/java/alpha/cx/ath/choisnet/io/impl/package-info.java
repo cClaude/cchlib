253e0e53877edbdc0dda7ae7e906df1c7256b5f6
Claude.CHOISNET@gmail.com <Claude.CHOISNET@gmail.com@bc589049-105d-1923-a3ce-6fcb24364ed1>
2011-04-01 15:40:14 +0000

    CHCLib (maven migration step 2)
    git-svn-id: https://cchlib.googlecode.com/svn/trunk@141 bc589049-105d-1923-a3ce-6fcb24364ed1

--- CHCLibCore/src/main/java/alpha/cx/ath/choisnet/io/impl/package-info.java
+++
@@ -1,8 +0,0 @@
-/**
- * Provide some tool to access to AlternateDataStream (Alpha)
- * <br/>
- * Theses implementations could be OS Specific
- * 
- * @author Claude CHOISNET
- */
-package alpha.cx.ath.choisnet.io.impl;
--- CHCLibCore/src/main/java/alpha/cx/ath/choisnet/net/dhcp/package-info.java
+++
@@ -1,6 +0,0 @@
-/**
- * Provided tools to deal with DHCP protocole
- *
- * @author Claude CHOISNET
- */
-package alpha.cx.ath.choisnet.net.dhcp;
