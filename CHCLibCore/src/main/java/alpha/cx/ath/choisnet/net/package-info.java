af4b06b6baf8895c8f9baace00b80c9fe4fc31ef
Claude.CHOISNET@gmail.com <Claude.CHOISNET@gmail.com@bc589049-105d-1923-a3ce-6fcb24364ed1>
2011-04-01 15:34:26 +0000

    CHCLib (maven migration step 2)
    git-svn-id: https://cchlib.googlecode.com/svn/trunk@139 bc589049-105d-1923-a3ce-6fcb24364ed1

--- CHCLibCore/src/main/java/alpha/cx/ath/choisnet/net/package-info.java
+++
@@ -1,6 +0,0 @@
-/**
- * Trying to offer a ping like operation for Java (Alpha)
- *
- * @author Claude CHOISNET
- */
-package alpha.cx.ath.choisnet.net;
--- CHCLibCore/src/main/java/cx/ath/choisnet/i18n/package-info.java
+++
@@ -1,6 +0,0 @@
-/**
- * Provide a solution for localization using reflection
- * 
- * @author Claude CHOISNET
- */
-package cx.ath.choisnet.i18n;
