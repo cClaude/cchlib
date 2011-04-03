dd3788256b785acd6fd3e8adf2ff81d4791514a9
Claude.CHOISNET@gmail.com <Claude.CHOISNET@gmail.com@bc589049-105d-1923-a3ce-6fcb24364ed1>
2011-04-03 04:52:31 +0000

    CHCLib - Prepare V4
    git-svn-id: https://cchlib.googlecode.com/svn/trunk@171 bc589049-105d-1923-a3ce-6fcb24364ed1

--- releases/chclibcore/install2maven.bat
+++ releases/chclibcore/install2maven.bat
@@ -1,5 +1,5 @@
-Set MYVER=03a.38.10
+Set MYVER=4.1.0-SNAPSHOT
 
-CALL mvn install:install-file -Dfile=CHCLibCore.%MYVER%.jar -DgroupId=com.googlecode.chclib -DartifactId=chclibcore -Dversion=%MYVER% -Dpackaging=jar
+CALL mvn install:install-file -Dfile=chclibcore-%MYVER%.jar -DgroupId=com.googlecode.chclib -DartifactId=chclibcore -Dversion=%MYVER% -Dpackaging=jar
 
 Pause
--- releases/chclibj2ee/install2maven.bat
+++ releases/chclibj2ee/install2maven.bat
@@ -1,4 +1,4 @@
-Set MYVER=03a.11.0
+Set MYVER=4.1.0-SNAPSHOT.jar
 
 CALL mvn install:install-file -Dfile=chclibj2ee-%MYVER%.jar -DgroupId=com.googlecode.chclib -DartifactId=chclibj2ee -Dversion=%MYVER% -Dpackaging=jar
 
