95020b97dc55621e49bb58e5488a08319ecaafb1
Claude.CHOISNET@gmail.com <Claude.CHOISNET@gmail.com@bc589049-105d-1923-a3ce-6fcb24364ed1>
2011-04-04 17:49:09 +0000

    git-svn-id: https://cchlib.googlecode.com/svn/trunk@172 bc589049-105d-1923-a3ce-6fcb24364ed1

--- releases/chclibj2ee/install2maven.bat
+++ releases/chclibj2ee/install2maven.bat
@@ -1,4 +1,4 @@
-Set MYVER=4.1.0-SNAPSHOT.jar
+Set MYVER=4.1.0-SNAPSHOT
 
 CALL mvn install:install-file -Dfile=chclibj2ee-%MYVER%.jar -DgroupId=com.googlecode.chclib -DartifactId=chclibj2ee -Dversion=%MYVER% -Dpackaging=jar
 
