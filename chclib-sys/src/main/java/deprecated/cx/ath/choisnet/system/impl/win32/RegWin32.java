fb8c82a944239896ddce96b38279ae4adaabd19c
Claude.CHOISNET@gmail.com <Claude.CHOISNET@gmail.com@bc589049-105d-1923-a3ce-6fcb24364ed1>
2011-04-20 14:42:11 +0000

    git-svn-id: https://cchlib.googlecode.com/svn/trunk@192 bc589049-105d-1923-a3ce-6fcb24364ed1

--- chclib-sys/src/main/java/deprecated/cx/ath/choisnet/system/impl/win32/RegWin32.java
+++ chclib-sys/src/main/java/deprecated/cx/ath/choisnet/system/impl/win32/RegWin32.java
@@ -18,6 +18,7 @@ import cx.ath.choisnet.util.enumeration.EnumerationHelper;
 /**
  * @author Claude CHOISNET
  */
+@Deprecated
 public class RegWin32
 {
     final private static Logger slogger = Logger.getLogger(RegWin32.class);
