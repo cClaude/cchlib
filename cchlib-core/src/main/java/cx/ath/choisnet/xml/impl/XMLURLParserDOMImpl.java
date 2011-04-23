46685f1c4d776ad2d4d50229b5c58eaffbbb47aa
Claude.CHOISNET <Claude.CHOISNET@bc589049-105d-1923-a3ce-6fcb24364ed1>
2011-04-23 16:19:18 +0000

    cchlib - 4.1.2
    git-svn-id: https://cchlib.googlecode.com/svn/trunk@540 bc589049-105d-1923-a3ce-6fcb24364ed1

---
+++ cchlib-core/src/main/java/cx/ath/choisnet/xml/impl/XMLURLParserDOMImpl.java
@@ -0,0 +1,46 @@
+package cx.ath.choisnet.xml.impl;
+
+import cx.ath.choisnet.xml.XMLParserErrorHandler;
+import cx.ath.choisnet.xml.XMLParserException;
+import cx.ath.choisnet.xml.XMLURLParser;
+import java.io.FileNotFoundException;
+import java.net.URL;
+import java.util.EnumSet;
+
+/**
+ * DOM parser implementation that allow to retrieve XML based URL for XML
+ *
+ * @author Claude CHOISNET
+ */
+public class XMLURLParserDOMImpl extends XMLParserDOMImpl
+    implements XMLURLParser
+{
+    private URL url;
+
+    /**
+     * TODO: doc!
+     *
+     * @param sourceFile
+     * @param attributes
+     * @param errorHandler
+     * @throws FileNotFoundException
+     * @throws XMLParserException
+     */
+    public XMLURLParserDOMImpl(
+            final URL                   sourceURL,
+            final XMLParserErrorHandler errorHandler,
+            final EnumSet<Attributs>    attributes
+            )
+        throws FileNotFoundException, XMLParserException
+    {
+        super( sourceURL, errorHandler, attributes );
+
+        url = sourceURL;
+    }
+
+    @Override
+    public URL getURL()
+    {
+        return url;
+    }
+}
