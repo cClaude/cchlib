42a9e6b34800ef4cd40ddb8ca3b21a802a6c29ef
Claude.CHOISNET <Claude.CHOISNET@bc589049-105d-1923-a3ce-6fcb24364ed1>
2011-06-17 07:04:02 +0000

    cchlib-j2ee - 4.1.5   Fix (according to evolutions of core): cx.ath.choisnet.servlet.debug.impl.InfosServletDisplayImpl2
    git-svn-id: https://cchlib.googlecode.com/svn/trunk@2018 bc589049-105d-1923-a3ce-6fcb24364ed1

--- cchlib-j2ee/src/main/java/cx/ath/choisnet/servlet/debug/impl/InfosServletDisplayImpl2.java
+++ cchlib-j2ee/src/main/java/cx/ath/choisnet/servlet/debug/impl/InfosServletDisplayImpl2.java
@@ -1,77 +1,78 @@
 package cx.ath.choisnet.servlet.debug.impl;
 
-import cx.ath.choisnet.lang.reflect.MappableHelper;
-import cx.ath.choisnet.lang.reflect.MappableHelperDefaultFactory;
-import cx.ath.choisnet.lang.reflect.MappableHelperFactory;
+import cx.ath.choisnet.lang.reflect.MappableBuilder;
+import cx.ath.choisnet.lang.reflect.MappableBuilderDefaultFactory;
+import cx.ath.choisnet.lang.reflect.MappableBuilderFactory;
+import cx.ath.choisnet.lang.reflect.MappableItem;
 import java.util.HashMap;
 import java.util.Map;
 
 /**
  * TODO: Doc
- * 
+ *
  * @author Claude CHOISNET
  */
 public class InfosServletDisplayImpl2 extends InfosServletDisplayImpl
 {
     /**
      * TODO: Doc
-     * 
+     *
      * @param title
      * @param anchorName
      * @param object
      */
     public InfosServletDisplayImpl2(
-            final String title, 
-            final String anchorName, 
+            final String title,
+            final String anchorName,
             final Object object
             )
     {
         super(
-            title, 
+            title,
             anchorName,
             InfosServletDisplayImpl2._toMap(object)
             );
     }
 
     /**
-     * 
+     *
      * TODO: Doc
      */
-    public static MappableHelperFactory getMappableHelperFactory()
+    public static MappableBuilderFactory getMappableBuilderFactory()
     {
-        return (new MappableHelperDefaultFactory())
+        return new MappableBuilderDefaultFactory()
             .setMethodesNamePattern("(get|is).*")
-            .addClasses(new Class[] { Object.class })
-            .addAttributes(new MappableHelper.Attributes[] { 
-                MappableHelper.Attributes.ALL_PRIMITIVE_TYPE, 
-                MappableHelper.Attributes.DO_ARRAYS, 
-                MappableHelper.Attributes.DO_ITERATOR, 
-                MappableHelper.Attributes.DO_ITERABLE, 
-                MappableHelper.Attributes.DO_ENUMERATION, 
-                MappableHelper.Attributes.DO_RECURSIVE
-                });
+            .add( Object.class )
+            .add(
+                    MappableItem.ALL_PRIMITIVE_TYPE,
+                    MappableItem.DO_ARRAYS,
+                    MappableItem.DO_ITERATOR,
+                    MappableItem.DO_ITERABLE,
+                    MappableItem.DO_ENUMERATION,
+                    MappableItem.DO_RECURSIVE
+                    );
     }
 
     private static final Map<String,String> _toMap(Object object)
     {
         return InfosServletDisplayImpl2._toMap(
-                InfosServletDisplayImpl2.getMappableHelperFactory(), 
+                InfosServletDisplayImpl2.getMappableBuilderFactory(),
                 object
                 );
     }
 
     private static final Map<String,String> _toMap(
-                final MappableHelperFactory   factory, 
+                final MappableBuilderFactory   factory,
                 final Object                  object
                 )
     {
-        return InfosServletDisplayImpl2._toMap(new MappableHelper(factory), object);
+        return InfosServletDisplayImpl2._toMap(new MappableBuilder(factory), object);
     }
 
     private static final Map<String,String> _toMap(
-                MappableHelper  aMappableHelper, 
-                Object          object
-                )
+            MappableBuilder aMappableHelper,
+            Object          object
+            )
     {
         if(object == null) {
             //throw new NullPointerException("'object' parameter is null");
@@ -79,6 +80,6 @@ public class InfosServletDisplayImpl2 extends InfosServletDisplayImpl
             }
         else {
             return aMappableHelper.toMap(object);
-        }
+            }
     }
 }
