6c834797e3cab69bdd89644be62094ac21dad136
Claude.CHOISNET <Claude.CHOISNET@bc589049-105d-1923-a3ce-6fcb24364ed1>
2012-01-15 20:02:32 +0000

    Sync
    git-svn-id: https://cchlib.googlecode.com/svn/trunk@2164 bc589049-105d-1923-a3ce-6fcb24364ed1

---
+++ cchlib-tools-duplicatefiles/src/main/java/cx/ath/choisnet/tools/analysis/FileResultFormater.java
@@ -0,0 +1,59 @@
+/**
+ *
+ */
+package cx.ath.choisnet.tools.analysis;
+
+import java.io.File;
+import java.text.DateFormat;
+import java.util.Date;
+import com.googlecode.cchlib.text.DateFormatHelper;
+
+/**
+ *
+ *
+ */
+public class FileResultFormater
+{
+    private final StringBuilder sb          = new StringBuilder();
+    private DateFormat          ISO_FORMAT  = DateFormatHelper.createFullDateISODateFormat();
+    private final String        space10chars;
+    /**
+     *
+     */
+    public FileResultFormater()
+    {
+        sb.setLength( 0 );
+
+        for( int i = 0; i<10; i++ ) {
+            sb.append( ' ' );
+            }
+        space10chars = sb.toString();
+
+    }
+
+    /**
+     *
+     * @param f
+     * @param prefix
+     * @return
+     */
+    public String format(
+        final File      f,
+        final String    prefix
+        )
+    {
+        final String fileLenStr = space10chars + f.length();
+
+        sb.setLength( 0 );
+        sb.append( prefix ); // F | D* | I*
+        sb.append( "|" );
+        sb.append( ISO_FORMAT.format( new Date( f.lastModified() ) ) );
+        sb.append( "|" );
+        sb.append( fileLenStr.substring( fileLenStr.length() - 10 ) );
+        sb.append( "||" ); // space for MD5
+        sb.append( f.getPath() );
+        sb.append( "\n" );
+
+        return sb.toString();
+    }
+}
