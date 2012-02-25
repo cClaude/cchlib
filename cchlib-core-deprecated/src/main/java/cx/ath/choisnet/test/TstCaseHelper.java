f223a7fe47e2cb161d090fbbe366a74b05b4259a
Claude.CHOISNET <Claude.CHOISNET@bc589049-105d-1923-a3ce-6fcb24364ed1>
2012-02-25 13:07:43 +0000

    cchlib - 4.1.7   Move some classes to deprecated project or sample project
    git-svn-id: https://cchlib.googlecode.com/svn/trunk@2316 bc589049-105d-1923-a3ce-6fcb24364ed1

---
+++ cchlib-core-deprecated/src/main/java/cx/ath/choisnet/test/TstCaseHelper.java
@@ -0,0 +1,76 @@
+package cx.ath.choisnet.test;
+
+import java.io.File;
+import java.io.FileFilter;
+import java.util.Iterator;
+import cx.ath.choisnet.io.FileFilterHelper;
+import cx.ath.choisnet.io.FileIterator;
+
+/**
+ * @deprecated use {@link AssertHelper} instead
+ */
+@Deprecated
+final
+public class TstCaseHelper
+{
+    private TstCaseHelper()
+    {//All static
+    }
+
+    /**
+     * use {@link AssertHelper#getTmpDirFile()} instead
+     */
+    @Deprecated
+    public final static File getTmpDirFile()
+    {
+        return new File( System.getProperty("java.io.tmpdir" ) );
+    }
+
+    /**
+     * use {@link AssertHelper#getUserHomeDirFile()} instead
+     */
+    @Deprecated
+    public final static File getUserHomeDirFile()
+    {
+        return new File( System.getProperty("user.home") );
+    }
+
+    /**
+     * use {@link AssertHelper#getSystemRootFile()} instead
+     */
+    @Deprecated
+    public final static File getSystemRootFile()
+    {
+        return new File( "/" );
+    }
+
+    /**
+     * use {@link AssertHelper#getFilesFrom(File, FileFilter)} instead
+     */
+    @Deprecated
+    public final static Iterator<File> getFilesFrom(
+            File        fileDirectory,
+            FileFilter  fileFilter
+            )
+    {
+        FileFilter justFileFilter = FileFilterHelper.not(
+                    FileFilterHelper.directoryFileFilter()
+                    );
+        FileFilter privateFileFilter;
+
+        if( fileFilter == null ) {
+            privateFileFilter = justFileFilter;
+        }
+        else {
+            privateFileFilter = FileFilterHelper.and(
+                    justFileFilter,
+                    fileFilter
+                    );
+        }
+
+        return new FileIterator(
+                fileDirectory,
+                privateFileFilter
+                );
+    }
+}
