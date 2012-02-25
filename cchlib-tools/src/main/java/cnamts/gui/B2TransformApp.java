a8337eefc28c22b3a77b87cf741cd74c48b65c57
Claude.CHOISNET <Claude.CHOISNET@bc589049-105d-1923-a3ce-6fcb24364ed1>
2012-02-25 22:53:53 +0000

    cchlib-tools - 4.1.7   Modifications in some proof of concept classes
    git-svn-id: https://cchlib.googlecode.com/svn/trunk@2320 bc589049-105d-1923-a3ce-6fcb24364ed1

--- cchlib-tools/src/main/java/cnamts/gui/B2TransformApp.java
+++ cchlib-tools/src/main/java/cnamts/gui/B2TransformApp.java
@@ -5,28 +5,22 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
 import java.net.URL;
+import javax.swing.JOptionPane;
 import org.apache.log4j.Logger;
 import cnamts.DelNonAlphaChar;
+import com.googlecode.cchlib.swing.DialogHelper;
 import com.googlecode.cchlib.swing.batchrunner.BatchRunnerInterruptedException;
 import com.googlecode.cchlib.swing.batchrunner.LazyBatchRunnerApp;
 
 /**
  *
  * @author Claude
- *
  */
 public class B2TransformApp extends LazyBatchRunnerApp
 {
     private static final transient Logger logger = Logger.getLogger( B2TransformApp.class );
     private B2TransformApp()
     {
-        super();
-//        super( new NeverFailResourceBundle(
-//            ResourceBundle.getBundle(
-//                B2TransformApp.class.getPackage().getName()
-//                    + ".DefaultLazyBatchRunnerAppResourceBundle"
-//                ) )
-//            );
     }
 
     /**
@@ -34,14 +28,18 @@ public class B2TransformApp extends LazyBatchRunnerApp
      */
     public static void main( final String[] args )
     {
-        B2TransformApp instance = new B2TransformApp();
+        try {
+            B2TransformApp instance = new B2TransformApp();
 
-        URL iconURL = B2TransformApp.class.getResource( "cnam_32x32.png" );
-        logger.info( "iconURL = " + iconURL );
+            URL iconURL = B2TransformApp.class.getResource( "cnam_32x32.png" );
+            logger.info( "iconURL = " + iconURL );
 
-        instance.start( iconURL );
+            instance.start( iconURL );
+            }
+        catch( Exception e ) {
+            DialogHelper.showMessageExceptionDialog( B2TransformApp.class.getName(), e );
+            }
     }
-
 
     //
     // LazyBatchRunner
@@ -59,14 +57,60 @@ public class B2TransformApp extends LazyBatchRunnerApp
     }
 
     @Override//LazyBatchRunner
-    public File buildDestinationFile( File sourceFile )
+    public File buildOuputFile( File sourceFile )
+            throws BatchRunnerInterruptedException
     {
-        final File destinationFolderFile = super.getDestinationFolderFile();
+        final File destinationFolderFile = super.getOutputFolderFile();
 
-        return new File(
+        File outputFile = new File(
                 destinationFolderFile,
-                sourceFile.getName() + ".128"
+                sourceFile.getName() + ".b2-128"
                 );
+
+        if( outputFile.exists() ) {
+            String message = String.format(
+                    "Le fichier %s existe déjà !\n"
+                    + "Souhaitez-vous le remplacer ?",
+                    outputFile
+                    );
+            String title = "Le fichier destination existe déjà.";
+            String[] choices = {
+                    "oui",
+                    "Non, renommer automatiquement",
+                    "Non, arrêter tous les traitements"
+                    };
+            int res = JOptionPane.showOptionDialog(
+                    getTopLevelWindow(),
+                    message,
+                    title,
+                    JOptionPane.YES_NO_CANCEL_OPTION,
+                    JOptionPane.WARNING_MESSAGE,
+                    null, // icon
+                    choices,
+                    choices[1]
+                    );
+
+            switch( res ) {
+                case JOptionPane.YES_OPTION :
+                    break;
+
+                case JOptionPane.NO_OPTION :
+                    throw new UnsupportedOperationException();
+                    //break;
+
+                //case JOptionPane.CANCEL_OPTION :
+                default : {
+                    BatchRunnerInterruptedException e
+                        = new BatchRunnerInterruptedException( "File already exist" );
+
+                    e.setCustomObject( outputFile );
+
+                    throw e;
+                    }
+                }
+            }
+
+        return outputFile;
     }
 
     @Override//LazyBatchRunner
@@ -102,68 +146,7 @@ public class B2TransformApp extends LazyBatchRunnerApp
     @Override//LazyBatchRunnerLocaleResources
     public String getTextDoAction()
     {
-        return "Convert";
+        return "Convertir";
     }
 
-/*
-    //
-    // LazyBatchRunnerLocaleResources
-    //
-
-    @Override//LazyBatchRunnerLocaleResources
-    public String getTextAddSourceFile()
-    {
-        return "Ajouter des fichiers source";
-    }
-    @Override//LazyBatchRunnerLocaleResources
-    public String getTextSetDestinationFolder()
-    {
-        return "Dossier de destination";
-    }
-    @Override//LazyBatchRunnerLocaleResources
-    public String getTextClearSourceFileList()
-    {
-        return "Effacer la liste";
-    }
-    @Override//LazyBatchRunnerLocaleResources
-    public String getTextDoAction()
-    {
-        return "Convert";
-    }
-    @Override//LazyBatchRunnerLocaleResources
-    public String getTextJFileChooserInitializerTitle()
-    {
-        return "Patientez...";
-    }
-    @Override//LazyBatchRunnerLocaleResources
-    public String getTextJFileChooserInitializerMessage()
-    {
-        return "Analyze de vos disques";
-    }
-    @Override//LazyBatchRunnerLocaleResources
-    public String getTextNoSourceFile()
-    {
-        return  "<<< Pas de fichiers source >>>";
-    }
-    @Override//LazyBatchRunnerLocaleResources
-    public String getTextNoDestinationFolder()
-    {
-        return "<<< Pas de dossier destination >>>";
-    }
-    @Override//LazyBatchRunnerLocaleResources
-    public String getTextWorkingIn()
-    {
-        return "Traitement de : %s";
-    }
-    @Override//LazyBatchRunnerLocaleResources
-    public String getTextEndOfBatch()
-    {
-        return "Traitement terminée";
-    }
-    @Override//LazyBatchRunnerLocaleResources
-    public String getTextFrameTitle()
-    {
-        return "Transformation B2 en fichier text 128 caractères par ligne";
-    }
-*/
 }
