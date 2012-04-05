0206c469fd51f7f9f35bff5ef5e7ab5896249fde
Claude.CHOISNET@gmail.com <Claude.CHOISNET@gmail.com@bc589049-105d-1923-a3ce-6fcb24364ed1>
2012-04-05 14:27:55 +0000

    git-svn-id: https://cchlib.googlecode.com/svn/trunk@2520 bc589049-105d-1923-a3ce-6fcb24364ed1

---
+++ cchlib-core-beta/src/main/java/com/googlecode/cchlib/misc/SplitMails.java
@@ -0,0 +1,109 @@
+package com.googlecode.cchlib.misc;
+
+import java.io.BufferedInputStream;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Iterator;
+import org.apache.log4j.Logger;
+
+/**
+ * com.googlecode.cchlib.apps.sample2.Sample2
+ */
+public class SplitMails implements Iterable<InputStream>
+{
+    public final static String TESTINFILE_REL_FROM_USERHOME = "Bureau/jup60/jup60.mails2";
+    public final static String TESTOUTDIR = "C:/temp/";
+    private final static Logger logger = Logger.getLogger( SplitMails.class );
+    private MailLineInputStream thunderbirdMails;
+
+    /**
+     *
+     * @param thunderbirdMailFile
+     * @throws FileNotFoundException
+     * @throws IOException
+     */
+    public SplitMails(
+        final File thunderbirdMailFile
+        )
+        throws FileNotFoundException, IOException
+    {
+        this.thunderbirdMails = new MailLineInputStream(
+                new BufferedInputStream(
+                    new FileInputStream( thunderbirdMailFile )
+                    )
+                );
+     }
+
+    /**
+     *
+     *
+     * /
+    public static void main( String[] args ) throws IOException
+    {
+
+        final File inFile        = new File( TESTINFILE );
+        final File outputDirFile = new File( TESTOUTDIR );
+
+        logger.info( "In File: " + inFile );
+        logger.info( "Out Dir: " + outputDirFile );
+
+        SplitMails instance  = new SplitMails( inFile );
+        int        mailCount = 0;
+
+
+        for( InputStream in : instance ) {
+            File outFile = new File( outputDirFile, Integer.toString( mailCount++ ) );
+            logger.info( "out File: " + outFile );
+
+//            OutputStream out = new BufferedOutputStream(
+//                new FileOutputStream( outFile )
+//                );
+
+            byte[] buffer = new byte[ 409600 ];
+            int    index  = 0;
+            int    c;
+
+            while( (c = in.read()) != -1 ) {
+                buffer[ index++ ] = (byte)c;
+                }
+
+            logger.info(
+                    String.format(
+                        "MSG (%d - %d):",
+                        index,
+                        instance.thunderbirdMails.getLineNumber(),
+                        new String( buffer, 0, index )
+                        )
+                    );
+            }
+
+        logger.info( "Done : mail count = " + mailCount );
+        //DialogHelper.showMessageExceptionDialog( "OK", new Exception( "OK") );
+    }*/
+
+    @Override
+    public Iterator<InputStream> iterator()
+    {
+        return new Iterator<InputStream>()
+            {
+                @Override
+                public boolean hasNext()
+                {
+                    return thunderbirdMails.isEOF();
+                }
+                @Override
+                public InputStream next()
+                {
+                    return new MailInputStream( thunderbirdMails );
+               }
+                @Override
+                public void remove()
+                {
+                    throw new UnsupportedOperationException();
+                }
+            };
+    }
+}
