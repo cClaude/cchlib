6f3bb753185f5a6ec1b81e2dbc6de55729570fc6
Claude.CHOISNET@gmail.com <Claude.CHOISNET@gmail.com@bc589049-105d-1923-a3ce-6fcb24364ed1>
2012-08-30 11:28:16 +0000

    cchlib-app-duplicatefilesmanager : new sub project
    git-svn-id: https://cchlib.googlecode.com/svn/trunk@2611 bc589049-105d-1923-a3ce-6fcb24364ed1

---
+++ cchlib-app-duplicatefilesmanager/src/test/java/com/googlecode/cchlib/apps/duplicatefiles/ResourcesLoaderTest.java
@@ -0,0 +1,59 @@
+package com.googlecode.cchlib.apps.duplicatefiles;
+
+import static org.junit.Assert.*;
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+public class ResourcesLoaderTest
+{
+
+    @BeforeClass
+    public static void setUpBeforeClass() throws Exception
+    {}
+
+    @AfterClass
+    public static void tearDownAfterClass() throws Exception
+    {}
+
+    @Before
+    public void setUp() throws Exception
+    {}
+
+    @After
+    public void tearDown() throws Exception
+    {}
+
+    @Test
+    public void testGetResource()
+    {
+        fail( "Not yet implemented" );
+    }
+
+    @Test
+    public void testGetResourceAsStream()
+    {
+        fail( "Not yet implemented" );
+    }
+
+    @Test
+    public void testGetImageIcon()
+    {
+        fail( "Not yet implemented" );
+    }
+
+    @Test
+    public void testGetImage()
+    {
+        fail( "Not yet implemented" );
+    }
+
+    @Test
+    public void testGetProperties()
+    {
+        fail( "Not yet implemented" );
+    }
+
+}
