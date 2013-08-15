93b3ebc15569bf0c9b874c0779305253d252f793
Claude.CHOISNET@gmail.com <Claude.CHOISNET@gmail.com@bc589049-105d-1923-a3ce-6fcb24364ed1>
2013-08-15 08:37:56 +0000

    cchlib-core-sample 1.4.8   Move some stuff
    git-svn-id: https://cchlib.googlecode.com/svn/trunk@8157 bc589049-105d-1923-a3ce-6fcb24364ed1

---
+++ cchlib-samples/src/main/java/com/googlecode/cchlib/samples/Samples.java
@@ -0,0 +1,26 @@
+package com.googlecode.cchlib.samples;
+
+import java.awt.Image;
+import org.apache.log4j.Logger;
+import com.googlecode.cchlib.resources.ResourcesLoader;
+import com.googlecode.cchlib.resources.ResourcesLoaderException;
+
+/**
+ * Just here to get {@link Samples}.class
+ */
+public class Samples 
+{
+    private final static Logger logger = Logger.getLogger( Samples.class );
+    private Samples() {} // All static
+
+    public static Image getSampleIconImage()
+    {
+        try {
+            return ResourcesLoader.getImage( Samples.class, "sample.png" );
+            }
+        catch( ResourcesLoaderException e ) {
+            logger.error( "Can't load 'sample.png'", e );
+            return null;
+            }
+   }
+}
---
+++ cchlib-samples/src/main/java/com/googlecode/cchlib/samples/swing/batchrunner/BatchRunnerSample.java
@@ -0,0 +1,127 @@
+package com.googlecode.cchlib.samples.swing.batchrunner;
+
+import java.awt.EventQueue;
+import java.awt.Image;
+import java.io.File;
+import java.io.IOException;
+import java.io.InputStream; // $codepro.audit.disable unnecessaryImport
+import java.io.OutputStream; // $codepro.audit.disable unnecessaryImport
+import com.googlecode.cchlib.swing.DialogHelper;
+import com.googlecode.cchlib.swing.batchrunner.AbstractBRRunnable;
+import com.googlecode.cchlib.swing.batchrunner.BRExecutionEvent;
+import com.googlecode.cchlib.swing.batchrunner.BRExecutionEventFactory;
+import com.googlecode.cchlib.swing.batchrunner.BRExecutionException;
+import com.googlecode.cchlib.swing.batchrunner.BRRunnable;
+import com.googlecode.cchlib.swing.batchrunner.ihm.BRPanelConfig;
+import com.googlecode.cchlib.swing.batchrunner.ihm.BRPanelLocaleResources;
+import com.googlecode.cchlib.swing.batchrunner.ihm.DefaultBRLocaleResources;
+import com.googlecode.cchlib.swing.batchrunner.ihm.DefaultBRPanelConfig;
+import com.googlecode.cchlib.swing.batchrunner.ihm.BRActionListener;
+import com.googlecode.cchlib.swing.batchrunner.ihm.BRFrame;
+import com.googlecode.cchlib.swing.batchrunner.impl.BRExecutionEventFactoryImpl;
+import com.googlecode.cchlib.swing.batchrunner.misc.BRXLocaleResources;
+
+public class BatchRunnerSample
+{
+    /**
+     * Launch the application.
+     */
+    public static void main( String[] args )
+    {
+        //final BRRunnable task = newSBRRunnableForFiles_DoesNothing();
+        final BRRunnable task = newSBRRunnableForStreams_CopyFile();
+
+        final BRPanelConfig             config                  = new DefaultBRPanelConfig();
+        final BRPanelLocaleResources    panelLocaleResources    = new DefaultBRLocaleResources();
+        final BRXLocaleResources        xLocaleResources        = new BRXLocaleResources() {
+            @Override
+            public String getProgressMonitorMessage()
+            {
+                return "Working....";
+            }
+            @Override
+            public String getFrameTitle()
+            {
+                return "Sample !";
+            }
+            @Override
+            public Image getFrameIconImage()
+            {
+                return null;
+            }
+        };
+ 
+        EventQueue.invokeLater( new Runnable() {
+            @Override
+            public void run()
+            {
+                try {
+                    BRFrame                 frame           = new BRFrame( panelLocaleResources, xLocaleResources );
+                    BRExecutionEventFactory eventFactory    = new BRExecutionEventFactoryImpl( frame, xLocaleResources );
+                    BRActionListener        actionListener  = new BRActionListener( eventFactory, task, config );
+
+                    frame.createFrame( actionListener );
+
+                    frame.setVisible( true );
+                    }
+                catch( Exception e ) {
+                    e.printStackTrace();
+
+                    DialogHelper.showMessageExceptionDialog( xLocaleResources.getFrameTitle(), e );
+                    }
+            }
+        } );
+    }
+
+    private static BRRunnable newSBRRunnableForStreams_CopyFile()
+    {
+        return new AbstractBRRunnable() {
+            @Override
+            public void execute( BRExecutionEvent event )
+                    throws BRExecutionException
+            {
+                logger.info( "DO execute()" );
+
+                try( InputStream is = event.getInputStream() ) {
+                    try( OutputStream os = event.getOutputStream() ) {
+                        assert is != null;
+                        assert os != null;
+
+                        byte[] buffer = new byte[256];
+                        int    len;
+
+                        while( (len = is.read( buffer )) != -1 ) {
+                            os.write( buffer, 0, len );
+                            }
+                        }
+                    }
+                catch( IOException e ) {
+                    throw new BRExecutionException( e );
+                    }
+
+                logger.info( "DONE" );
+            }
+        };
+    }
+
+    private static void sleep( int millis )
+    {
+        try { Thread.sleep( millis ); } catch( InterruptedException ignore ) {} // $codepro.audit.disable emptyCatchClause, logExceptions
+    }
+
+    private static BRRunnable newSBRRunnableForFiles_DoesNothing()
+    {
+        return new AbstractBRRunnable() {
+            @Override
+            public void execute( BRExecutionEvent event )
+                    throws BRExecutionException
+            {
+                File sourceFile      = event.getSourceFile();
+                File destinationFile = event.getDestinationFile();
+
+                logger.info( "DO execute() : " + sourceFile + " -> " + destinationFile );
+                sleep( 10 * 1000 );
+                logger.info( "DONE execute() : " + sourceFile + " -> " + destinationFile );
+            }};
+    }
+}
