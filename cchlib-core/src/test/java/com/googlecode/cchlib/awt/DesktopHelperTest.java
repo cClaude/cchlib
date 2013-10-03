295f3d6bacab1078460182d5d6360f623e3cc12d
Claude.CHOISNET <Claude.CHOISNET@bc589049-105d-1923-a3ce-6fcb24364ed1>
2013-10-03 18:26:35 +0000

    cchlib-core 4.1.8 : Restore DesktopHelperTest
    git-svn-id: https://cchlib.googlecode.com/svn/trunk@8188 bc589049-105d-1923-a3ce-6fcb24364ed1

--- cchlib-core/src/test/java/com/googlecode/cchlib/awt/DesktopHelperTest.java
+++ cchlib-core/src/test/java/com/googlecode/cchlib/awt/DesktopHelperTest.java
@@ -2,26 +2,30 @@ package com.googlecode.cchlib.awt;
 
 import java.net.MalformedURLException;
 import java.net.URL;
-import org.junit.Ignore;
+import org.apache.log4j.Logger;
 import org.junit.Test;
 
 public class DesktopHelperTest
 {
+    private final static Logger logger = Logger.getLogger(DesktopHelperTest.class);
+
     @Test
-    @Ignore // result depend of jdk
+    @Deprecated
     public void test_isNativeDesktopSupported()
-        throws PlateformeDesktopNotSupportedException
     {
-        DesktopHelper.isNativeDesktopSupported();
+        try {
+            DesktopHelper.isNativeDesktopSupported();
+        } catch (PlateformeDesktopNotSupportedException e) {
+            logger.info( e.getMessage() );
+        }
     }
-    
+
     @Test
-    @Ignore // could not work under jdk 1.5
     public void testDesktopHelper()
         throws PlateformeDesktopNotSupportedException, MalformedURLException
-    {        
+    {
         URL url = new URL( "https://code.google.com/p/cchlib/" );
-        
+
         DesktopHelper.browse( url  );
     }
 }
