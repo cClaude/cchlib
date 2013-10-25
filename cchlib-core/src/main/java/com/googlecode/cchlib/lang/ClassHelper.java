e7f214773d3edbbc445146000d1b514294a566f5
Claude.CHOISNET@gmail.com <Claude.CHOISNET@gmail.com@bc589049-105d-1923-a3ce-6fcb24364ed1>
2013-10-25 11:15:04 +0000

    Fix
    git-svn-id: https://cchlib.googlecode.com/svn/trunk@8202 bc589049-105d-1923-a3ce-6fcb24364ed1

---
+++ cchlib-core/src/main/java/com/googlecode/cchlib/lang/ClassHelper.java
@@ -0,0 +1,23 @@
+package com.googlecode.cchlib.lang;
+
+/**
+ * TODOC
+ *
+ */
+public final class ClassHelper
+{
+    private ClassHelper() {}
+
+    /**
+     * TODOC
+     * @param anObject
+     * @return TODOC
+     */
+    public static <E> Class<E> getClass( final E anObject )
+    {
+        @SuppressWarnings("unchecked")
+        final Class<E> result = (Class<E>)(anObject == null ? null : anObject.getClass());
+        return result;
+    }
+
+}
