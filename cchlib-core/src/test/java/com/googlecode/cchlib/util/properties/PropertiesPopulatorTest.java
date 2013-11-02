893309038fc8f3a6853eb7843fb7adf590f91853
Claude.CHOISNET@gmail.com <Claude.CHOISNET@gmail.com@bc589049-105d-1923-a3ce-6fcb24364ed1>
2013-11-02 17:15:58 +0000

    Fix + Audit + CleanUp
    git-svn-id: https://cchlib.googlecode.com/svn/trunk@8216 bc589049-105d-1923-a3ce-6fcb24364ed1

--- cchlib-core/src/test/java/com/googlecode/cchlib/util/properties/PropertiesPopulatorTest.java
+++ cchlib-core/src/test/java/com/googlecode/cchlib/util/properties/PropertiesPopulatorTest.java
@@ -50,7 +50,6 @@ public class PropertiesPopulatorTest
         }
     }
 
-    @SuppressWarnings("deprecation")
     @Test
     public void test_PropertiesPopulator1() throws PopulatorException
     {
