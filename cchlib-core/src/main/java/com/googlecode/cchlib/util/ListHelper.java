cde7f81441f9a7481388739b2a28a50c6ffa9ca7
Claude.CHOISNET@gmail.com <Claude.CHOISNET@gmail.com@bc589049-105d-1923-a3ce-6fcb24364ed1>
2014-02-20 19:48:10 +0000

    Minor fix
    git-svn-id: https://cchlib.googlecode.com/svn/trunk@8304 bc589049-105d-1923-a3ce-6fcb24364ed1

--- cchlib-core/src/main/java/com/googlecode/cchlib/util/ListHelper.java
+++ cchlib-core/src/main/java/com/googlecode/cchlib/util/ListHelper.java
@@ -1,6 +1,6 @@
 package com.googlecode.cchlib.util;
 
-import java.util.AbstractList;
+import java.util.ArrayList;
 import java.util.Collection;
 import java.util.Collections;
 import java.util.List;
@@ -17,34 +17,10 @@ public final class ListHelper
             list = (List<T>)collection;
             }
         else {
-            list = new ListFromCollection<T>(collection);
+            list = new ArrayList<T>(collection);
             }
 
         return Collections.unmodifiableList( list );
      }
 
-    private static final class ListFromCollection<T> extends AbstractList<T>
-    {
-        private T[] array;
-
-        public ListFromCollection( final Collection<T> collection )
-        {
-            @SuppressWarnings("unchecked")
-            final T[] genArray = (T[])new Object[collection.size()];
-            this.array = collection.toArray( genArray  );
-        }
-
-        @Override
-        public T get( final int index )
-        {
-            return array[ index ];
-        }
-
-        @Override
-        public int size()
-        {
-            return array.length;
-        }
-
-    }
 }
