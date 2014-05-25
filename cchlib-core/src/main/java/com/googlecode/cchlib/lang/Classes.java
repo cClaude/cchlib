c3cfc2670ed388ea24adacdff9ad8e427beb1de8
Claude CHOISNET <Claude.CHOISNET@nespresso.com>
2014-05-25 20:03:55 +0200

    chlib-core: add Classes#getSimpleName(Class<T>) and Classes#getClassSimpleName(T)

--- cchlib-core/src/main/java/com/googlecode/cchlib/lang/Classes.java
+++ cchlib-core/src/main/java/com/googlecode/cchlib/lang/Classes.java
@@ -26,10 +26,37 @@ public final class Classes
     }
 
     /**
+     * Safely return the class simple name of a Class
+     *
+     * @param <E> Type of the object
+     * @param aClass The class, could be null
+     * @return simple name of the class object or null if <code>anObject</code> is null.
+     * @since 4.2
+     */
+    public static <E> String getSimpleName( @Nullable final Class<E> aClass )
+    {
+        return  aClass != null ? aClass.getSimpleName() : null;
+    }
+
+    /**
+     * Safely return the class simple name of an object
+     *
+     * @param <E> Type of the object
+     * @param anObject Object to get the class
+     * @return simple name of the class object or null if <code>anObject</code> is null.
+     * @since 4.2
+     */
+    public static <E> String getClassSimpleName( @Nullable final E anObject )
+    {
+        return  anObject != null ? anObject.getClass().getSimpleName() : null;
+    }
+
+    /**
      * @return an empty array of classes
      */
     public static Class<?>[] emptyArray()
     {
         return EMPTY_ARRAY;
     }
+
 }
