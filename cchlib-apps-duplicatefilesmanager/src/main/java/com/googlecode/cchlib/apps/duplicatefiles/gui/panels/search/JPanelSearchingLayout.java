021632e6264b04b92df11b9d4f874848330893f3
Claude CHOISNET <Claude.CHOISNET@nespresso.com>
2014-06-01 12:52:59 +0200

    cchlib-apps-duplicatefilesmanager : Prépare next layout

--- cchlib-apps-duplicatefilesmanager/src/main/java/com/googlecode/cchlib/apps/duplicatefiles/gui/panels/search/JPanelSearchingLayout.java
+++ cchlib-apps-duplicatefilesmanager/src/main/java/com/googlecode/cchlib/apps/duplicatefiles/gui/panels/search/JPanelSearchingLayout.java
@@ -54,100 +54,116 @@ abstract class JPanelSearchingLayout extends JPanelSearchingDisplayI18n implemen
 
         final GridBagLayout gridBagLayout = new GridBagLayout();
         gridBagLayout.columnWidths = new int[]{0, 0, 0};
-        gridBagLayout.rowHeights = new int[]{60, 0, 0, 0, 0, 0, 0};
+        gridBagLayout.rowHeights = new int[]{0, 0, 0, 0, 30, 30, 0};
         gridBagLayout.columnWeights = new double[]{1.0, 1.0, Double.MIN_VALUE};
-        gridBagLayout.rowWeights = new double[]{1.0, 0.0, 0.0, 0.0, 0.0, 2.0, Double.MIN_VALUE};
+        gridBagLayout.rowWeights = new double[]{0.0, 0.0, 0.0, 0.0, 1.0, 2.0, Double.MIN_VALUE};
         setLayout(gridBagLayout);
 
-        this.panelCurrentFiles = new JPanelCurrentFiles( nThreads );
-        final GridBagConstraints gbc_panelCurrentFile = new GridBagConstraints();
-        gbc_panelCurrentFile.gridwidth = 2;
-        gbc_panelCurrentFile.insets = new Insets(0, 0, 5, 5);
-        gbc_panelCurrentFile.fill = GridBagConstraints.BOTH;
-        gbc_panelCurrentFile.gridx = 0;
-        gbc_panelCurrentFile.gridy = 0;
-        add(this.panelCurrentFiles, gbc_panelCurrentFile);
+        {// line 0
+            final JLabel lblFilesReads = new JLabel("Files reads :");
+            lblFilesReads.setHorizontalAlignment(SwingConstants.RIGHT);
+            final GridBagConstraints gbc_lblFilesReads = new GridBagConstraints();
+            gbc_lblFilesReads.fill = GridBagConstraints.HORIZONTAL;
+            gbc_lblFilesReads.insets = new Insets(0, 0, 5, 5);
+            gbc_lblFilesReads.gridx = 0;
+            gbc_lblFilesReads.gridy = 0;
+            add(lblFilesReads, gbc_lblFilesReads);
+        }
+        {// line 0
+            jProgressBarFiles = new JProgressBar();
+            final GridBagConstraints gbc_jProgressBarFiles = new GridBagConstraints();
+            gbc_jProgressBarFiles.fill = GridBagConstraints.HORIZONTAL;
+            gbc_jProgressBarFiles.insets = new Insets(0, 0, 5, 0);
+            gbc_jProgressBarFiles.gridx = 1;
+            gbc_jProgressBarFiles.gridy = 0;
+            add(getjProgressBarFiles(), gbc_jProgressBarFiles);
+        }
 
-        final JLabel lblFilesReads = new JLabel("Files reads :");
-        lblFilesReads.setHorizontalAlignment(SwingConstants.RIGHT);
-        final GridBagConstraints gbc_lblFilesReads = new GridBagConstraints();
-        gbc_lblFilesReads.fill = GridBagConstraints.HORIZONTAL;
-        gbc_lblFilesReads.insets = new Insets(0, 0, 5, 5);
-        gbc_lblFilesReads.gridx = 0;
-        gbc_lblFilesReads.gridy = 1;
-        add(lblFilesReads, gbc_lblFilesReads);
+        {// line 1
+            jLabelBytesReadFromDisk = new JLabel("Bytes reads :");
+            jLabelBytesReadFromDisk.setHorizontalAlignment(SwingConstants.RIGHT);
+            final GridBagConstraints gbc_jLabelBytesReadFromDisk = new GridBagConstraints();
+            gbc_jLabelBytesReadFromDisk.fill = GridBagConstraints.HORIZONTAL;
+            gbc_jLabelBytesReadFromDisk.insets = new Insets(0, 0, 5, 5);
+            gbc_jLabelBytesReadFromDisk.gridx = 0;
+            gbc_jLabelBytesReadFromDisk.gridy = 1;
+            add(jLabelBytesReadFromDisk, gbc_jLabelBytesReadFromDisk);
+        }
+        {// line 1
+            jProgressBarOctets = new JProgressBar();
+            final GridBagConstraints gbc_jProgressBarOctets = new GridBagConstraints();
+            gbc_jProgressBarOctets.fill = GridBagConstraints.HORIZONTAL;
+            gbc_jProgressBarOctets.insets = new Insets(0, 0, 5, 0);
+            gbc_jProgressBarOctets.gridx = 1;
+            gbc_jProgressBarOctets.gridy = 1;
+            add(jProgressBarOctets, gbc_jProgressBarOctets);
+        }
 
-        jProgressBarFiles = new JProgressBar();
-        final GridBagConstraints gbc_jProgressBarFiles = new GridBagConstraints();
-        gbc_jProgressBarFiles.fill = GridBagConstraints.HORIZONTAL;
-        gbc_jProgressBarFiles.insets = new Insets(0, 0, 5, 0);
-        gbc_jProgressBarFiles.gridx = 1;
-        gbc_jProgressBarFiles.gridy = 1;
-        add(getjProgressBarFiles(), gbc_jProgressBarFiles);
+        {// line 2
+            jLabelDuplicateSetsFound = new JLabel("Duplicate sets found :");
+            jLabelDuplicateSetsFound.setHorizontalAlignment(SwingConstants.RIGHT);
+            final GridBagConstraints gbc_jLabelDuplicateSetsFound = new GridBagConstraints();
+            gbc_jLabelDuplicateSetsFound.fill = GridBagConstraints.HORIZONTAL;
+            gbc_jLabelDuplicateSetsFound.insets = new Insets(0, 0, 5, 5);
+            gbc_jLabelDuplicateSetsFound.gridx = 0;
+            gbc_jLabelDuplicateSetsFound.gridy = 2;
+            add(jLabelDuplicateSetsFound, gbc_jLabelDuplicateSetsFound);
+        }
+        {// line 2
+            jLabelDuplicateSetsFoundValue = new JLabel();
+            final GridBagConstraints gbc_jLabelDuplicateSetsFoundValue = new GridBagConstraints();
+            gbc_jLabelDuplicateSetsFoundValue.fill = GridBagConstraints.HORIZONTAL;
+            gbc_jLabelDuplicateSetsFoundValue.insets = new Insets(0, 0, 5, 0);
+            gbc_jLabelDuplicateSetsFoundValue.gridx = 1;
+            gbc_jLabelDuplicateSetsFoundValue.gridy = 2;
+            add(jLabelDuplicateSetsFoundValue, gbc_jLabelDuplicateSetsFoundValue);
+        }
 
-        jLabelBytesReadFromDisk = new JLabel("Bytes reads :");
-        jLabelBytesReadFromDisk.setHorizontalAlignment(SwingConstants.RIGHT);
-        final GridBagConstraints gbc_jLabelBytesReadFromDisk = new GridBagConstraints();
-        gbc_jLabelBytesReadFromDisk.fill = GridBagConstraints.HORIZONTAL;
-        gbc_jLabelBytesReadFromDisk.insets = new Insets(0, 0, 5, 5);
-        gbc_jLabelBytesReadFromDisk.gridx = 0;
-        gbc_jLabelBytesReadFromDisk.gridy = 2;
-        add(jLabelBytesReadFromDisk, gbc_jLabelBytesReadFromDisk);
+        {// line 3
+            jLabelDuplicateFilesFound = new JLabel();
+            jLabelDuplicateFilesFound.setHorizontalAlignment(SwingConstants.RIGHT);
+            jLabelDuplicateFilesFound.setText("DuplicateFilesFound :");
+            final GridBagConstraints gbc_jLabelDuplicateFilesFound = new GridBagConstraints();
+            gbc_jLabelDuplicateFilesFound.fill = GridBagConstraints.HORIZONTAL;
+            gbc_jLabelDuplicateFilesFound.insets = new Insets(0, 0, 5, 5);
+            gbc_jLabelDuplicateFilesFound.gridx = 0;
+            gbc_jLabelDuplicateFilesFound.gridy = 3;
+            add(jLabelDuplicateFilesFound, gbc_jLabelDuplicateFilesFound);
+        }
+        {// line 3
+            jLabelDuplicateFilesFoundValue = new JLabel();
+            jLabelDuplicateFilesFoundValue.setHorizontalAlignment(SwingConstants.LEFT);
+            final GridBagConstraints gbc_jLabelDuplicateFilesFoundValue = new GridBagConstraints();
+            gbc_jLabelDuplicateFilesFoundValue.fill = GridBagConstraints.BOTH;
+            gbc_jLabelDuplicateFilesFoundValue.insets = new Insets(0, 0, 5, 0);
+            gbc_jLabelDuplicateFilesFoundValue.gridx = 1;
+            gbc_jLabelDuplicateFilesFoundValue.gridy = 3;
+            add(jLabelDuplicateFilesFoundValue, gbc_jLabelDuplicateFilesFoundValue);
+        }
 
-        jProgressBarOctets = new JProgressBar();
-        final GridBagConstraints gbc_jProgressBarOctets = new GridBagConstraints();
-        gbc_jProgressBarOctets.fill = GridBagConstraints.HORIZONTAL;
-        gbc_jProgressBarOctets.insets = new Insets(0, 0, 5, 0);
-        gbc_jProgressBarOctets.gridx = 1;
-        gbc_jProgressBarOctets.gridy = 2;
-        add(jProgressBarOctets, gbc_jProgressBarOctets);
+        {// line 4
+            this.panelCurrentFiles = new JPanelCurrentFiles( nThreads );
+            final GridBagConstraints gbc_panelCurrentFile = new GridBagConstraints();
+            gbc_panelCurrentFile.gridwidth = 2;
+            gbc_panelCurrentFile.insets = new Insets(0, 0, 5, 0);
+            gbc_panelCurrentFile.fill = GridBagConstraints.BOTH;
+            gbc_panelCurrentFile.gridx = 0;
+            gbc_panelCurrentFile.gridy = 4;
+            add(this.panelCurrentFiles, gbc_panelCurrentFile);
+        }
 
-        jLabelDuplicateSetsFound = new JLabel("Duplicate sets found :");
-        jLabelDuplicateSetsFound.setHorizontalAlignment(SwingConstants.RIGHT);
-        final GridBagConstraints gbc_jLabelDuplicateSetsFound = new GridBagConstraints();
-        gbc_jLabelDuplicateSetsFound.fill = GridBagConstraints.HORIZONTAL;
-        gbc_jLabelDuplicateSetsFound.insets = new Insets(0, 0, 5, 5);
-        gbc_jLabelDuplicateSetsFound.gridx = 0;
-        gbc_jLabelDuplicateSetsFound.gridy = 3;
-        add(jLabelDuplicateSetsFound, gbc_jLabelDuplicateSetsFound);
+        {// line 5
+            final JScrollPane scrollPane = new JScrollPane();
+            final GridBagConstraints gbc_scrollPane = new GridBagConstraints();
+            gbc_scrollPane.fill = GridBagConstraints.BOTH;
+            gbc_scrollPane.gridwidth = 2;
+            gbc_scrollPane.gridx = 0;
+            gbc_scrollPane.gridy = 5;
+            add(scrollPane, gbc_scrollPane);
 
-        jLabelDuplicateSetsFoundValue = new JLabel("");
-        final GridBagConstraints gbc_jLabelDuplicateSetsFoundValue = new GridBagConstraints();
-        gbc_jLabelDuplicateSetsFoundValue.fill = GridBagConstraints.HORIZONTAL;
-        gbc_jLabelDuplicateSetsFoundValue.insets = new Insets(0, 0, 5, 0);
-        gbc_jLabelDuplicateSetsFoundValue.gridx = 1;
-        gbc_jLabelDuplicateSetsFoundValue.gridy = 3;
-        add(jLabelDuplicateSetsFoundValue, gbc_jLabelDuplicateSetsFoundValue);
-
-        jLabelDuplicateFilesFound = new JLabel();
-        jLabelDuplicateFilesFound.setHorizontalAlignment(SwingConstants.RIGHT);
-        jLabelDuplicateFilesFound.setText("DuplicateFilesFound :");
-        final GridBagConstraints gbc_jLabelDuplicateFilesFound = new GridBagConstraints();
-        gbc_jLabelDuplicateFilesFound.fill = GridBagConstraints.HORIZONTAL;
-        gbc_jLabelDuplicateFilesFound.insets = new Insets(0, 0, 5, 5);
-        gbc_jLabelDuplicateFilesFound.gridx = 0;
-        gbc_jLabelDuplicateFilesFound.gridy = 4;
-        add(jLabelDuplicateFilesFound, gbc_jLabelDuplicateFilesFound);
-
-        jLabelDuplicateFilesFoundValue = new JLabel();
-        jLabelDuplicateFilesFoundValue.setHorizontalAlignment(SwingConstants.LEFT);
-        final GridBagConstraints gbc_jLabelDuplicateFilesFoundValue = new GridBagConstraints();
-        gbc_jLabelDuplicateFilesFoundValue.fill = GridBagConstraints.HORIZONTAL;
-        gbc_jLabelDuplicateFilesFoundValue.insets = new Insets(0, 0, 5, 0);
-        gbc_jLabelDuplicateFilesFoundValue.gridx = 1;
-        gbc_jLabelDuplicateFilesFoundValue.gridy = 4;
-        add(jLabelDuplicateFilesFoundValue, gbc_jLabelDuplicateFilesFoundValue);
-
-        final JScrollPane scrollPane = new JScrollPane();
-        final GridBagConstraints gbc_scrollPane = new GridBagConstraints();
-        gbc_scrollPane.fill = GridBagConstraints.BOTH;
-        gbc_scrollPane.gridwidth = 2;
-        gbc_scrollPane.gridx = 0;
-        gbc_scrollPane.gridy = 5;
-        add(scrollPane, gbc_scrollPane);
-
-        jTableErrorList = new JTable();
-        scrollPane.setViewportView(jTableErrorList);
+            jTableErrorList = new JTable();
+            scrollPane.setViewportView(jTableErrorList);
+        }
     }
 
     protected int getNumberOfThreads()
