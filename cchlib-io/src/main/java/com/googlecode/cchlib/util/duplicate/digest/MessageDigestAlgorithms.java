70382f48daf158f03e8a9447f33afb66fc64f2db
Claude CHOISNET <Claude.CHOISNET@GMail.com>
2016-09-13 12:29:31 +0200

    Improve MessageDigestAlgorithms and add unit tests

--- cchlib-io/src/main/java/com/googlecode/cchlib/util/duplicate/digest/MessageDigestAlgorithms.java
+++ cchlib-io/src/main/java/com/googlecode/cchlib/util/duplicate/digest/MessageDigestAlgorithms.java
@@ -7,7 +7,10 @@ public enum MessageDigestAlgorithms {
 
     MD5("MD5"),
     SHA_1("SHA-1"),
+    SHA_224("SHA-224"),
     SHA_256("SHA-256"),
+    SHA_384("SHA-384"),
+    SHA_512("SHA-512"),
     ;
 
     private String algorithm;
