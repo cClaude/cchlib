c510bcf9c5411b37c1ecb33dd25ec193d61d9f49
CClaude <Claude.CHOISNET@gmail.com>
2016-11-24 12:38:36 +0100

    Add documentation to tests

--- apps/cchlib-apps-duplicatefilesmanager/src/test/java/com/googlecode/cchlib/apps/duplicatefiles/console/ConsoleAppTest.java
+++ apps/cchlib-apps-duplicatefilesmanager/src/test/java/com/googlecode/cchlib/apps/duplicatefiles/console/ConsoleAppTest.java
@@ -5,36 +5,39 @@ import org.junit.Test;
 import com.googlecode.cchlib.apps.duplicatefiles.ConsoleApp;
 import com.googlecode.cchlib.io.FileHelper;
 
-public class ConsoleAppTest 
+public class ConsoleAppTest
 {
     private static final String RESOURCE_PATH = "src/test/resources/";
-    private static final String LOG_PATH      = ".logs/";
+    private static final String LOG_PATH      = "testResults/";
 
     @Test
     public void testJSONConfig1() throws NoSuchAlgorithmException
     {
-        ConsoleApp.main( createArgs(  "shareCC-1.json", "json-filefilter-1.json" ) );
+        ConsoleApp.main( createArgs( "shareCC-1.json", "json-filefilter-1.json" ) );
     }
 
     @Test
     public void testJSONConfig2() throws NoSuchAlgorithmException
     {
-        ConsoleApp.main( createArgs(  "shareCC-2.json", "json-filefilter-2.json" ) );
+        ConsoleApp.main( createArgs( "shareCC-2.json", "json-filefilter-2.json" ) );
     }
 
     @Test
     public void testJSONConfig3() throws NoSuchAlgorithmException
     {
-        ConsoleApp.main( createArgs(  "shareCC-3.json", "json-filefilter-3.json" ) );
+        ConsoleApp.main( createArgs( "shareCC-3.json", "json-filefilter-3.json" ) );
     }
 
-    private String[] createArgs( final String outputJSonFilename, final String inputJSonFilename )
+    private String[] createArgs(
+        final String outputJSonFilename,
+        final String inputJSonFilename
+        )
     {
-        final String[] params = {
-                "-directory",FileHelper.getTmpDirFile().getPath(),
-                "-json", LOG_PATH + outputJSonFilename,
-                "--files-filter-from-file", RESOURCE_PATH + inputJSonFilename,
+        return new String[] {
+            // Run test using /tmp folder
+            "-directory", FileHelper.getTmpDirFile().getPath(),
+            "-json", LOG_PATH + outputJSonFilename,
+            "--files-filter-from-file", RESOURCE_PATH + inputJSonFilename,
         };
-        return params;
     }
 }
