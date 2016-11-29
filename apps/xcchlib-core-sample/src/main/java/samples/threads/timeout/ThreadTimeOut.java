31c6396f6ebbeac6d1aa14eac2a58575a11f1e6c
CClaude <Claude.CHOISNET@gmail.com>
2016-11-29 18:55:31 +0100

    Threads.sleep

--- apps/xcchlib-core-sample/src/main/java/samples/threads/timeout/ThreadTimeOut.java
+++ apps/xcchlib-core-sample/src/main/java/samples/threads/timeout/ThreadTimeOut.java
@@ -9,30 +9,30 @@ import java.util.concurrent.TimeoutException;
 
 public class ThreadTimeOut
 {
-    static class AnyException extends Exception 
+    static class AnyException extends Exception
     {
         private static final long serialVersionUID = 1L;
     }
 
-    static class CustomTask implements Callable<String> 
+    static class CustomTask implements Callable<String>
     {
         @Override
         public String call() throws AnyException, InterruptedException
         {
-            Thread.sleep(4000); // Just to demo a long running task of 4 seconds.
+            Thread.sleep( 4_000 ); // Just to demo a long running task of 4 seconds.
             return "Ready!";
         }
     }
 
-    public static void main(String[] args) throws Exception {
-        ExecutorService executor = Executors.newSingleThreadExecutor();
-        Future<String> future = executor.submit(new CustomTask());
+    public static void main(final String[] args) throws Exception {
+        final ExecutorService executor = Executors.newSingleThreadExecutor();
+        final Future<String> future = executor.submit(new CustomTask());
 
         try {
             System.out.println("Started..");
             System.out.println(future.get(3, TimeUnit.SECONDS));
             System.out.println("Finished!");
-        } catch (TimeoutException e) {
+        } catch (final TimeoutException e) {
             System.out.println("Terminated!");
         }
 
