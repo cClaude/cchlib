a6c11c4d634b202b279299fe939a7976aab5e3a8
CClaude <Claude.CHOISNET@gmail.com>
2016-12-01 22:37:29 +0100

    Improve build to track javadoc issues

--- buildAll.sh
+++ buildAll.sh
@@ -8,11 +8,17 @@ cat .logs/mvn-install.log | grep "warning: no description for" | sort | uniq > .
 cat .logs/mvn-install.log | grep "warning: no @param for" | sort | uniq > .logs/mvn-install-warn-no-param.log
 cat .logs/mvn-install.log | grep "warning: no @return" | sort | uniq > .logs/mvn-install-warn-no-return.log
 cat .logs/mvn-install.log | grep "error: unknown tag:" | sort | uniq > .logs/mvn-install-error-unknown-tag.log
+cat .logs/mvn-install.log | grep "error: exception not thrown:" | sort | uniq > .logs/mvn-install-error-not-thrown.log
+cat .logs/mvn-install.log | grep "warning: empty" | sort | uniq > .logs/mvn-install-warning-empty.log
+cat .logs/mvn-install.log | grep "error: reference not found" | sort | uniq > .logs/mvn-install-error-reference-not-found.log
 
 cat .logs/mvn-install.log | grep -v "warning: no description for" \
   | grep -v "warning: no @param for" \
   | grep -v "warning: no @return" \
   | grep -v "error: unknown tag:" \
+  | grep -v "error: exception not thrown:" \
+  | grep -v "warning: empty" \
+  | grep -v "error: reference not found" \
   > .logs/mvn-install-others.log
 
 pushd apps
