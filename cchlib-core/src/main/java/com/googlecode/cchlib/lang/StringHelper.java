3af2e819dcacde6da04ab5393a8b94b9a9020a4c
CClaude <Claude.CHOISNET@gmail.com>
2016-12-04 19:30:18 +0100

    Add isNullOrEmpty(String)

--- cchlib-core/src/main/java/com/googlecode/cchlib/lang/StringHelper.java
+++ cchlib-core/src/main/java/com/googlecode/cchlib/lang/StringHelper.java
@@ -1,6 +1,8 @@
 package com.googlecode.cchlib.lang;
 
 import java.util.ArrayList;
+import javax.annotation.Nonnull;
+import javax.annotation.Nullable;
 
 /**
  *
@@ -13,6 +15,11 @@ public final class StringHelper
 
     private StringHelper() {} // ALl static
 
+    /**
+     * Returns an empty String array
+     * @return an empty String array
+     */
+    @Nonnull
     public static String[] emptyArray()
     {
         return EMPTY_ARRAY;
@@ -20,10 +27,13 @@ public final class StringHelper
 
     /**
      * Returns the given string if it is non-null; the empty string otherwise
-     * @param str the string to test and possibly return
+     *
+     * @param str
+     *            the string to test and possibly return
      * @return string itself if it is non-null; {@link #EMPTY} if it is null
      */
-    public static String nullToEmpty( final String str )
+    @Nonnull
+    public static String nullToEmpty( @Nullable final String str )
     {
         return (str == null) ? EMPTY : str;
     }
@@ -31,17 +41,21 @@ public final class StringHelper
     /**
      * Splits this string around matches of the given token
      * <p>
-     * This method work exactly like {@link #split(String, String)} but
-     * is optimized for token based on character.
-     * </p>
+     * This method work exactly like {@link #split(String, String)}
+     * but is optimized for token based on character.
      *
-     * @param str   String to split
-     * @param token Char token to use as delimiter.
-     * @return the array of strings computed by splitting this string around matches of the given token
-     * @throws NullPointerException if str is null
+     * @param str
+     *            String to split
+     * @param token
+     *            Char token to use as delimiter.
+     * @return the array of strings computed by splitting this string
+     *         around matches of the given token
+     * @throws NullPointerException
+     *             if {@code str} is null
      * @see #split(String, String)
      */
-    public static String[] split( final String str, final char token )
+    @Nonnull
+    public static String[] split( @Nonnull final String str, final char token )
     {
         if( str == null ) {
             throw new NullPointerException();
@@ -72,27 +86,44 @@ public final class StringHelper
      * <p>
      * This method differ from method {@link String#split(String)}
      * Trailing empty strings are included in the resulting array.
-     * </p>
-     * <br>
-     * <br>
-     * The string "boo:and:foo", for example, yields the following results with these tokens:
-     * <blockquote>
+     * <p>
+     * The string "boo:and:foo", for example, yields the following
+     * results with these tokens:
      * <table border="2" cellpadding="1" cellspacing="0">
      * <caption>Example</caption>
-     * <tr><th>token</th><th>Result</th></tr>
-     * <tr><td>:</td><td>{"boo","and","foo"};</td></tr>
-     * <tr><td>o</td><td>{"b","",":and:f","",""}</td></tr>
-     * <tr><td>b</td><td>{"","oo:and:foo"}</td></tr>
+     * <tr>
+     * <th>token</th>
+     * <th>Result</th>
+     * </tr>
+     * <tr>
+     * <td>:</td>
+     * <td>{"boo","and","foo"};</td>
+     * </tr>
+     * <tr>
+     * <td>o</td>
+     * <td>{"b","",":and:f","",""}</td>
+     * </tr>
+     * <tr>
+     * <td>b</td>
+     * <td>{"","oo:and:foo"}</td>
+     * </tr>
      * </table>
-     * </blockquote>
      *
-     * @param str   String to split
-     * @param token Char token to use as delimiter.
-     * @return the array of strings computed by splitting this string around matches of the given token
-     * @throws NullPointerException if str or token is null
+     * @param str
+     *            String to split
+     * @param token
+     *            Char token to use as delimiter.
+     * @return the array of strings computed by splitting this string
+     *         around matches of the given token
+     * @throws NullPointerException
+     *             if {@code str} or {@code token} is null
      * @see #split(String, char)
      */
-    public static String[] split( final String str, final String token )
+    @Nonnull
+    public static String[] split(
+        @Nonnull final String str,
+        @Nonnull final String token
+        )
     {
         if( str == null ) {
             throw new NullPointerException();
@@ -116,5 +147,22 @@ public final class StringHelper
         final String[] res = new String[ array.size() ];
 
         return array.toArray(res);
+    }
+
+    /**
+     * Returns true if {@code str} is null or if length() is 0.
+     *
+     * @param str
+     *            String to evaluate
+     * @return true if String is null or if length() is 0, otherwise false
+     * @since 4.2
+     */
+    public static boolean isNullOrEmpty( @Nullable final String str )
+    {
+        if( str == null ) {
+            return true;
+        }
+
+        return str.isEmpty();
     }
 }
