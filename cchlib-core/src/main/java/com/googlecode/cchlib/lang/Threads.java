ef11b62d39b73ce960387edbdc0edc09c522296b
CClaude <Claude.CHOISNET@gmail.com>
2016-12-07 12:22:56 +0100

    Add Thread#start methods

--- cchlib-core/src/main/java/com/googlecode/cchlib/lang/Threads.java
+++ cchlib-core/src/main/java/com/googlecode/cchlib/lang/Threads.java
@@ -1,10 +1,14 @@
 package com.googlecode.cchlib.lang;
 
+import javax.annotation.Nonnull;
+
 /**
  * Thread related tools
  */
 public class Threads
 {
+    private static final String TARGET_IS_NULL = "target is null";
+
     private Threads()
     {
         // All static
@@ -55,4 +59,50 @@ public class Threads
         }
     }
 
+    /**
+     * Create a new {@link Thread} and start it.
+     * <p>
+     * Simply call {@code new Thread( target, threadName ).start();}
+     * to have less hard reading lambda expressions.
+     *
+     * @param threadName
+     *            the name of the new thread
+     * @param target
+     *            the object whose run method is invoked when this
+     *            thread is started.
+     * @since 4.2
+     */
+    public static void start(
+        final String            threadName,
+        @Nonnull final Runnable target
+        )
+    {
+        if( target == null ) {
+            throw new IllegalArgumentException( TARGET_IS_NULL );
+        }
+
+        new Thread( target, threadName ).start();
+    }
+
+    /**
+     * Create a new {@link Thread} and start it.
+     * <p>
+     * Simply call {@code new Thread( target, threadName ).start();}
+     * to have less hard reading lambda expressions.
+     *
+     * @param target
+     *            the object whose run method is invoked when this
+     *            thread is started.
+     * @since 4.2
+     */
+    public static void start(
+        @Nonnull final Runnable target
+        )
+    {
+        if( target == null ) {
+            throw new IllegalArgumentException( TARGET_IS_NULL );
+        }
+
+        new Thread( target ).start();
+    }
 }
