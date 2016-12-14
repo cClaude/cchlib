92ade75773d6b20a65e0921d0974ce95e91171b1
CClaude <Claude.CHOISNET@gmail.com>
2016-12-15 00:20:56 +0100

    Improve tests

--- cchlib-core-test/src/test/java/com/googlecode/cchlib/VersionTest.java
+++ cchlib-core-test/src/test/java/com/googlecode/cchlib/VersionTest.java
@@ -1,22 +1,43 @@
 package com.googlecode.cchlib;
 
+import static org.fest.assertions.api.Assertions.assertThat;
 import java.io.IOException;
 import java.text.ParseException;
-import org.junit.Assert;
+import java.util.Date;
+import java.util.concurrent.TimeUnit;
+import org.apache.log4j.Logger;
 import org.junit.Test;
 
-public class VersionTest {
+public class VersionTest
+{
+    private static final Logger LOGGER = Logger.getLogger( VersionTest.class );
 
     @Test
     public void testVersion() throws IOException, ParseException
     {
         final Version version = Version.getInstance();
 
-        Assert.assertNotNull( "new Version()", version );
-        Assert.assertNotNull( "version.getName()", version.getName() );
-        Assert.assertNotNull( "version.getVersion()", version.getVersion() );
-        Assert.assertNotNull( "version", version.getDate() );
-        Assert.assertNotNull( "version.toString()", version.toString() );
+        LOGGER.info( "Version is :" + version );
+
+        assertThat( version ).as( "new Version()" ).isNotNull();
+        assertThat( version.getName() )
+            .as( "version.getName()" )
+            .isNotNull()
+            .doesNotContain( "${project.name}" )
+            .isEqualTo( "cchlib-core" );
+        assertThat( version.getVersion() )
+            .as( "version.getVersion()" )
+            .isNotNull()
+            .doesNotContain( "${project.version}" );
+
+        final Date now                 = new Date();
+        final long deltaInMilliseconds = TimeUnit.DAYS.toMillis( 1 ); // One day
+
+        assertThat( version.getDate() )
+            .as( "version.getDate()" )
+            .isNotNull().isCloseTo( now, deltaInMilliseconds );
+
+        assertThat( version.toString() ).as( "version.toString()" ).isNotNull();
     }
 
     @Test
@@ -24,7 +45,7 @@ public class VersionTest {
     {
         final String[] args = null;
 
-        // Just test if this call work
+        // Just test if this call work (entry point valid)
         Version.main( args );
     }
 }
