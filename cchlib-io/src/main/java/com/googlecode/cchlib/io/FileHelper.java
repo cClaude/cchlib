a1d718264db91f503c48230a66c3ad3f7723515b
CClaude <Claude.CHOISNET@gmail.com>
2016-12-15 13:18:48 +0100

    Typo

--- cchlib-io/src/main/java/com/googlecode/cchlib/io/FileHelper.java
+++ cchlib-io/src/main/java/com/googlecode/cchlib/io/FileHelper.java
@@ -153,8 +153,8 @@ public final class FileHelper
      * creates a new directory somewhere beneath the system's temporary
      * directory (as defined by the java.io.tmpdir
      * system property), and returns its name.
-     *
-     * <p>This method assumes that the temporary volume is writable,
+     * <p>
+     * This method assumes that the temporary volume is writable,
      * has free inodes and free blocks.
      *
      * @return the newly-created directory
