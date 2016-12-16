3a827a709aa26f52fc4e05557c3e47fb31ea22b9
CClaude <Claude.CHOISNET@gmail.com>
2016-12-16 14:45:15 +0100

    Improve message when File is not a directory

--- cchlib-io/src/main/java/com/googlecode/cchlib/io/FileIterable.java
+++ cchlib-io/src/main/java/com/googlecode/cchlib/io/FileIterable.java
@@ -5,63 +5,78 @@ import java.io.FileFilter;
 import java.util.Iterator;
 
 /**
+ * Create an {@link Iterable} base on a directory
  *
  * @since 4.1.8
  */
 public class FileIterable implements Iterable<File>
 {
-    private File        rootFolderFile;
-    private FileFilter  fileFilter;
-    private FileFilter  directoryFilter;
+    private final File        rootFolderFile;
+    private final FileFilter  fileFilter;
+    private final FileFilter  directoryFilter;
 
     /**
      * Create a FileIterable starting from rootFolderFile
      *
-     * @param rootFolderFile root File directory for this Iterator
-     * @throws NullPointerException if rootFolderFile is null
+     * @param rootFolderFile
+     *            root File directory for this Iterator
+     * @throws NullPointerException
+     *             if rootFolderFile is null
+     * @throws IllegalArgumentException
+     *             if rootFolderFile is not a directory
      */
-    public FileIterable(File rootFolderFile)
+    public FileIterable( final File rootFolderFile )
     {
-        this(rootFolderFile,null,null);
+        this( rootFolderFile, null, null );
     }
 
     /**
-     * Create a FileIterable starting from rootFolderFile,
-     * with giving {@link FileFilter} to filter File result.
+     * Create a FileIterable starting from rootFolderFile, with giving
+     * {@link FileFilter} to filter File result.
      *
-     * @param rootFolderFile    Root File directory for this Iterator
-     * @param fileFilter        File filter to select files (any File object)
-     *                          than should be in result (could be null).
-     * @throws NullPointerException if rootFolderFile is null
+     * @param rootFolderFile
+     *            Root File directory for this Iterator
+     * @param fileFilter
+     *            File filter to select files (any File object) than should
+     *            be in result (could be null).
+     * @throws NullPointerException
+     *             if rootFolderFile is null
+     * @throws IllegalArgumentException
+     *             if rootFolderFile is not a directory
      */
     public FileIterable(
-            File        rootFolderFile,
-            FileFilter  fileFilter
-            )
+        final File       rootFolderFile,
+        final FileFilter fileFilter
+        )
     {
-        this(rootFolderFile,fileFilter,null);
+        this( rootFolderFile, fileFilter, null );
     }
 
     /**
-     * Create a FileIterator starting from rootFolderFile,
-     * with giving {@link FileFilter}.
+     * Create a FileIterator starting from rootFolderFile, with giving
+     * {@link FileFilter}.
      *
-     * @param rootFolderFile    Root File directory for this Iterator
-     * @param fileFilter        File filter to select files (any File object)
-     *                          than should be in result (could be null).
-     * @param directoryFilter   File filter to select directories than should
-     *                          be explored (could be null) .
-     * @throws NullPointerException if rootFolderFile is null
-     * @throws IllegalArgumentException if rootFolderFile is not a directory
+     * @param rootFolderFile
+     *            Root File directory for this Iterator
+     * @param fileFilter
+     *            File filter to select files (any File object) than should
+     *            be in result (could be null).
+     * @param directoryFilter
+     *            File filter to select directories than should be explored
+     *            (could be null) .
+     * @throws NullPointerException
+     *             if rootFolderFile is null
+     * @throws IllegalArgumentException
+     *             if rootFolderFile is not a directory
      */
     public FileIterable(
-        final File rootFolderFile,
+        final File       rootFolderFile,
         final FileFilter fileFilter,
         final FileFilter directoryFilter
         )
     {
-        if( !rootFolderFile.isDirectory() ) {
-            throw new IllegalArgumentException();
+        if( ! rootFolderFile.isDirectory() ) {
+            throw new IllegalArgumentException( "Not a directory: " + rootFolderFile );
             }
 
         this.rootFolderFile  = rootFolderFile;
@@ -72,6 +87,10 @@ public class FileIterable implements Iterable<File>
     @Override
     public Iterator<File> iterator()
     {
-        return new FileIterator( rootFolderFile, fileFilter, directoryFilter );
+        return new FileIterator(
+            this.rootFolderFile,
+            this.fileFilter,
+            this.directoryFilter
+            );
     }
 }
