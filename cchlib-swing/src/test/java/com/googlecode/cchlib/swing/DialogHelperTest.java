bcbd780be6f0d5cb8837caa28852636ed10c31e2
CClaude <Claude.CHOISNET@gmail.com>
2016-12-16 20:51:59 +0100

    Simplify tests

--- cchlib-swing/src/test/java/com/googlecode/cchlib/swing/DialogHelperTest.java
+++ cchlib-swing/src/test/java/com/googlecode/cchlib/swing/DialogHelperTest.java
@@ -1,80 +1,67 @@
 package com.googlecode.cchlib.swing;
 
+import static org.fest.assertions.api.Assertions.assertThat;
 import static org.junit.Assume.assumeTrue;
+import java.awt.Window;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.TimeUnit;
 import javax.swing.AbstractButton;
 import javax.swing.JButton;
 import org.apache.log4j.Logger;
 import org.junit.Test;
+import com.googlecode.cchlib.lang.Threads;
 
 public class DialogHelperTest
 {
-    private static final Logger LOGGER = Logger.getLogger( DialogHelperTest.class );
-
-    @Test
-    public void test_showMessageExceptionDialog1()
+    private static class FakeException extends Exception
     {
-        // Stop if GUI usage is not allowed
-        assumeTrue( SafeSwingUtilities.isSwingAvailable() );
+        private static final long serialVersionUID = 1L;
 
-        runtest_showMessageExceptionDialog( e -> DialogHelper.showMessageExceptionDialog( "Just for testing", e ));
-    }
-
-    @Test
-    public void test_showMessageExceptionDialog2()
-    {
-        // Stop if GUI usage is not allowed
-        assumeTrue( SafeSwingUtilities.isSwingAvailable() );
-
-        runtest_showMessageExceptionDialog( e -> {
-            final AbstractButton[] buttons = new AbstractButton[5];
-
-            for( int i=0; i<buttons.length; i++ ) {
-                buttons[ i ] = new JButton( "Button " + i );
-                }
-
-            final int selectedIndex = DialogHelper.showMessageExceptionDialog(
-                null,
-                "Just for testing",
-                e,
-                buttons
-                );
-
-            LOGGER.info( "selected index: " + selectedIndex );
-        });
-    }
-
-    private interface OpenExceptionDialog
-    {
-        void showMessageExceptionDialog( Throwable e );
-    }
-
-    private void runtest_showMessageExceptionDialog(
-        final OpenExceptionDialog openExceptionDialog
-        )
-    {
-        try { crashTest(); } catch( final FakeException e ) {
-            final Exception exception = e;
-            new Thread( (Runnable)( ) -> {
-                try {
-                    openExceptionDialog.showMessageExceptionDialog( exception );
-                    }
-                catch( final Exception e1 ) {
-                    LOGGER.fatal( "showMessageExceptionDialog Real ERROR!!!", e1 );
-                    }
-            },"test").start();
-            }
-    }
-
-    private static class FakeException extends Exception {
         public FakeException( final String message )
         {
             super( message );
         }
+
         public FakeException( final String message, final Throwable cause )
         {
             super( message, cause );
         }
-        private static final long serialVersionUID = 1L;
+    }
+
+    private static final Logger LOGGER = Logger.getLogger( DialogHelperTest.class );
+
+    @Test
+    public void test_showMessageExceptionDialog()
+        throws InterruptedException, ExecutionException
+    {
+        // Stop if GUI usage is not allowed
+        assumeTrue( SafeSwingUtilities.isSwingAvailable() );
+
+        final boolean result = Threads.startAndWait(
+                () -> showMessageExceptionDialog(),
+                3,
+                TimeUnit.SECONDS
+                );
+
+        assertThat( result ).isTrue();
+    }
+
+    @Test
+    public void test_showMessageExceptionDialog_with_extra_button()
+        throws InterruptedException, ExecutionException
+    {
+        // Stop if GUI usage is not allowed
+        assumeTrue( SafeSwingUtilities.isSwingAvailable() );
+
+        final int result = Threads.startAndWait(
+                () -> showMessageExceptionDialog_with_extra_button(),
+                3,
+                TimeUnit.SECONDS
+                );
+
+        LOGGER.info( "selected button index : " + result );
+
+        assertThat( result ).isGreaterThan( -1 );
     }
 
     private static void crashLoop( final int i ) throws FakeException
@@ -95,5 +82,51 @@ public class DialogHelperTest
         catch( final FakeException e ) {
             throw new FakeException( "Fake  Level 2", e );
             }
+    }
+
+    private final static boolean showMessageExceptionDialog()
+    {
+        try {
+            crashTest();
+
+            return false;
+            }
+        catch( final FakeException cause ) {
+            DialogHelper.showMessageExceptionDialog( "Just for testing", cause );
+
+            return true;
+        }
+    }
+
+    private final static int showMessageExceptionDialog_with_extra_button()
+    {
+        try {
+            crashTest();
+
+            return -1;
+            }
+        catch( final FakeException cause ) {
+            final AbstractButton[] buttons = new AbstractButton[] {
+                    new JButton( "Test A" ),
+                    new JButton( "Test B" ),
+                    new JButton( "Test C" ),
+                    new JButton( "Test D" ),
+            };
+            return DialogHelper.showMessageExceptionDialog(
+                   (Window)null,
+                   "Just for testing",
+                   cause,
+                   buttons );
+        }
+    }
+
+    /**
+     * Test result directly from CLI to validate "copy" menu
+     *
+     * @param args Nothing
+     */
+    public final static void main( final String[] args )
+    {
+        showMessageExceptionDialog();
     }
 }
