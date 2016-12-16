68875733dbaa0ab19833f7eebf41d637b6087b1a
CClaude <Claude.CHOISNET@gmail.com>
2016-12-16 16:58:24 +0100

    Disable some tests if swing is not avialable

--- cchlib-swing/src/test/java/com/googlecode/cchlib/resources/ResourcesLoaderTest.java
+++ cchlib-swing/src/test/java/com/googlecode/cchlib/resources/ResourcesLoaderTest.java
@@ -1,12 +1,14 @@
 package com.googlecode.cchlib.resources;
 
+import static org.fest.assertions.api.Assertions.assertThat;
+import static org.junit.Assume.assumeTrue;
 import java.awt.Image;
 import java.io.IOException;
 import java.io.InputStream;
 import java.net.URL;
 import javax.swing.Icon;
-import org.junit.Assert;
 import org.junit.Test;
+import com.googlecode.cchlib.swing.SafeSwingUtilities;
 
 @SuppressWarnings("resource")
 public class ResourcesLoaderTest
@@ -14,35 +16,47 @@ public class ResourcesLoaderTest
     @Test
     public void test_getResource()
     {
-        final URL url = ResourcesLoader.getResource( ResourcesLoader.OK_ICON_32x32 );
+        final URL actual = ResourcesLoader.getResource( ResourcesLoader.OK_ICON_32x32 );
 
-        Assert.assertNotNull( url );
+        assertThat( actual )
+            .as( "Can not find: " + ResourcesLoader.OK_ICON_32x32 )
+            .isNotNull();
     }
 
     @Test
     public void test_getResourceAsStream()
         throws ResourcesLoaderException, IOException
     {
-        final InputStream is = ResourcesLoader.getResourceAsStream( ResourcesLoader.OK_ICON_32x32 );
-
-        Assert.assertNotNull( is );
-        is.close();
+        try( final InputStream is = ResourcesLoader.getResourceAsStream( ResourcesLoader.OK_ICON_32x32 ) ) {
+            assertThat( is )
+                .as( "Can not find: " + ResourcesLoader.OK_ICON_32x32 )
+                .isNotNull();
+        };
     }
 
     @Test
     public void test_getImageIcon() throws ResourcesLoaderException
     {
+        // Stop if GUI usage is not allowed
+        assumeTrue( SafeSwingUtilities.isSwingAvailable() );
+
         final Icon icon = ResourcesLoader.getImageIcon( ResourcesLoader.OK_ICON_32x32 );
 
-        Assert.assertNotNull( icon );
+        assertThat( icon )
+            .as( "Can not find: " + ResourcesLoader.OK_ICON_32x32 )
+            .isNotNull();
     }
 
     @Test
     public void test_getImage() throws ResourcesLoaderException
     {
+        // Stop if GUI usage is not allowed
+        assumeTrue( SafeSwingUtilities.isSwingAvailable() );
+
         final Image image = ResourcesLoader.getImage( ResourcesLoader.OK_ICON_32x32 );
 
-        Assert.assertNotNull( image );
+        assertThat( image )
+            .as( "Can not find: " + ResourcesLoader.OK_ICON_32x32 )
+            .isNotNull();
     }
-
 }
--- cchlib-swing/src/test/java/com/googlecode/cchlib/swing/textfield/XTextFieldTest.java
+++ cchlib-swing/src/test/java/com/googlecode/cchlib/swing/textfield/XTextFieldTest.java
@@ -1,9 +1,12 @@
 package com.googlecode.cchlib.swing.textfield;
 
+import static org.junit.Assume.assumeTrue;
 import java.util.Locale;
 import org.junit.Assert;
+import org.junit.Before;
 import org.junit.Test;
 import com.googlecode.cchlib.i18n.core.AutoI18nCore;
+import com.googlecode.cchlib.swing.SafeSwingUtilities;
 import com.googlecode.cchlib.swing.i18n.SwingAutoI18nCoreFactory;
 
 /**
@@ -12,6 +15,13 @@ import com.googlecode.cchlib.swing.i18n.SwingAutoI18nCoreFactory;
  */
 public class XTextFieldTest
 {
+    @Before
+    public void setUp()
+    {
+        // Stop if GUI usage is not allowed
+        assumeTrue( SafeSwingUtilities.isSwingAvailable() );
+    }
+
     @Test
     public void testNoI18n()
     {
