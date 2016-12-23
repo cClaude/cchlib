8942f1468ebe5195013fca41e77545eb46f4a747
CClaude <Claude.CHOISNET@gmail.com>
2016-12-23 20:26:08 +0100

    Add tests

--- cchlib-io-test/src/test/java/com/googlecode/cchlib/io/checksum/XMD5Test.java
+++ cchlib-io-test/src/test/java/com/googlecode/cchlib/io/checksum/XMD5Test.java
@@ -7,14 +7,21 @@ import java.io.IOException;
 import org.apache.log4j.Logger;
 import org.junit.Before;
 import org.junit.Test;
+import com.googlecode.cchlib.util.CancelRequestException;
+import com.googlecode.cchlib.util.duplicate.digest.FileDigest;
+import com.googlecode.cchlib.util.duplicate.digest.FileDigestListener;
+import com.googlecode.cchlib.util.duplicate.digest.MessageDigestAlgorithms;
 
 public class XMD5Test
 {
     private static final Logger LOGGER = Logger.getLogger( XMD5Test.class );
 
+    private static final int bufferSize = 2048;
+
     @SuppressWarnings("deprecation")
     private com.googlecode.cchlib.util.duplicate.XMessageDigestFile mdf;
     private Iterable<File>                                          fileList;
+    private FileDigest                                              fdMd5;
 
     @Before
     @SuppressWarnings("deprecation")
@@ -22,10 +29,11 @@ public class XMD5Test
     {
         this.mdf      = new com.googlecode.cchlib.util.duplicate.XMessageDigestFile( "MD5" );
         this.fileList = TestMD5Helper.createTestFiles();
+        this.fdMd5    = MessageDigestAlgorithms.MD5.newFileDigest( bufferSize );
     }
 
     @Test
-    public void test_getHashString() throws IOException
+    public void test_getHashString() throws IOException, CancelRequestException
     {
         for( final File file : this.fileList ) {
             try {
@@ -37,25 +45,51 @@ public class XMD5Test
         }
     }
 
-    private void test_getHashString( final File file ) throws IOException
+    private void test_getHashString( final File file ) throws IOException, CancelRequestException
     {
         final long lastModified = file.lastModified();
         final long length       = file.length();
 
         final String hashString1 = MD5.getHashString( file ).toUpperCase();
         final String hashString2 = computeOldVersionHashString( file ).toUpperCase();
+        final String hashString3 = computeFileDigest( file ).toUpperCase();
 
         if( ToolBox.fileNotChanged( file, lastModified, length ) ) {
             LOGGER.info(
                     "File: " + file +
                     " MD5(1)" + hashString1 +
-                    " MD5(2)" + hashString2
+                    " MD5(2)" + hashString2 +
+                    " MD5(3)" + hashString3
                     );
 
             assertThat( hashString1 )
                 .as( "Bad checksum for file: " + file )
-                .isEqualTo( hashString2 );
+                .isEqualTo( hashString2 )
+                .isEqualTo( hashString3 );
         }
+    }
+
+    private String computeFileDigest( final File file ) throws IOException, CancelRequestException
+    {
+        final FileDigestListener listener = new FileDigestListener()
+        {
+            @Override
+            public void computeDigest( final File file, final int length )
+            {
+                // Empty
+            }
+
+            @Override
+            public boolean isCancel()
+            {
+                return false;
+            }
+
+        };
+
+        this.fdMd5.computeFile( file, listener );
+
+        return this.fdMd5.digestString();
     }
 
     private byte[] computeOldVersionHash( final File file ) throws IOException
