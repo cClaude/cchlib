601463fd01e675452ce201b9cc4b135d2338e9cc
CClaude <Claude.CHOISNET@gmail.com>
2016-12-25 00:33:09 +0100

    Cleaning

--- apps/xcchlib-tools/src/main/java/com/googlecode/cchlib/tools/downloader/comboconfig/GenericDownloaderAppComboBoxConfig.java
+++ apps/xcchlib-tools/src/main/java/com/googlecode/cchlib/tools/downloader/comboconfig/GenericDownloaderAppComboBoxConfig.java
@@ -3,39 +3,11 @@ package com.googlecode.cchlib.tools.downloader.comboconfig;
 import java.util.List;
 import com.googlecode.cchlib.tools.downloader.GenericDownloaderUIPanelEntry;
 
-/**
- * TODOC
- *
- */
 public interface GenericDownloaderAppComboBoxConfig
 {
-   /**
-    *
-    * @return TODOC
-    */
    int getSelectedIndex();
-
-   /**
-    *
-    * @param selectedIndex
-    */
    void setSelectedIndex( int selectedIndex );
-
-   /**
-    *
-    * @return TODOC
-    */
    String getComboBoxSelectedValue();
-
-   /**
-   *
-   * @return TODOC
-   */
    String getDescription();
-
-   /**
-   *
-   * @return TODOC
-   */
    List<GenericDownloaderUIPanelEntry.Item> getJComboBoxEntry();
 }
