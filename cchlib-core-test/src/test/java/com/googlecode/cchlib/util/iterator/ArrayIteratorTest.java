b67a5b408d2b57514964ee3c1659194131711846
CClaude <Claude.CHOISNET@gmail.com>
2016-12-25 13:19:41 +0100

    Improve tests

--- cchlib-core-test/src/test/java/com/googlecode/cchlib/util/iterator/ArrayIteratorTest.java
+++ cchlib-core-test/src/test/java/com/googlecode/cchlib/util/iterator/ArrayIteratorTest.java
@@ -1,6 +1,6 @@
-// $codepro.audit.disable logExceptions, avoidAutoBoxing, importOrder, numericLiterals
 package com.googlecode.cchlib.util.iterator;
 
+import static org.fest.assertions.api.Assertions.assertThat;
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
 import static org.junit.Assert.fail;
@@ -9,7 +9,6 @@ import java.util.Iterator;
 import java.util.List;
 import java.util.NoSuchElementException;
 import org.apache.log4j.Logger;
-import org.junit.Assert;
 import org.junit.Test;
 
 /**
@@ -24,42 +23,52 @@ public class ArrayIteratorTest
         1,2,3,4,5,6
     };
     private static final List<?>[] ARRAY_LIST = {
-        buildList(1),
-        buildList(2),
-        buildList(3),
-        buildList(4)
+        buildList( 1 ),
+        buildList( 2 ),
+        buildList( 3 ),
+        buildList( 4 )
     };
 
     @Test
     public void test1()
     {
-        final ArrayIterator<Integer> iter  = new ArrayIterator<Integer>(ARRAY_INT);
-        int                    count = 0;
+        final Iterator<Integer> iter  = new ArrayIterator<Integer>( ARRAY_INT );
+
+        int count         = 0;
+        int expectedValue = 1;
 
         while( iter.hasNext() ) {
-            iter.next();
+            final Integer actual = iter.next();
+            assertThat( actual ).isEqualTo( expectedValue );
+
+            expectedValue++;
             count++;
         }
 
-        Assert.assertEquals("Not same size !", ARRAY_INT.length, count);
+        assertThat( count ).as( "Not same size !" ).isEqualTo( ARRAY_INT.length );
     }
 
     @Test
     public void test2()
     {
-        final int offset = 2;
-        final int len    = 3;
-        final ArrayIterator<Integer> iter  = new ArrayIterator<Integer>(ARRAY_INT,offset,len);
-        int                    count = 0;
-        Integer                firstValue = null;
+        final int                    offset = 2;
+        final int                    len    = 3;
+        final ArrayIterator<Integer> iter   = new ArrayIterator<Integer>( ARRAY_INT, offset, len );
+
+        Integer firstValue    = null;
+        int     count         = 0;
+        int     expectedValue = ARRAY_INT[ offset ];
 
         while( iter.hasNext() ) {
-            final Integer v = iter.next();
+            final Integer actual = iter.next();
 
             if( firstValue == null ) {
-                firstValue = v;
+                firstValue = actual;
             }
 
+            assertThat( actual ).isEqualTo( expectedValue );
+
+            expectedValue++;
             count++;
         }
 
@@ -98,7 +107,7 @@ public class ArrayIteratorTest
             }
             fail( "Should fail" );
         }
-        catch( final NoSuchElementException ignore ) { // $codepro.audit.disable emptyCatchClause
+        catch( final NoSuchElementException ignore ) {
             //ok
         }
 
@@ -115,7 +124,10 @@ public class ArrayIteratorTest
         test_BuildFromItems( Integer.class, iter, 3 );
     }
 
-    public <T> void test_BuildFromItems( final Class<T> clazz, final T[] items)
+    private static <T> void test_BuildFromItems(
+        final Class<T>  clazz,
+        final T[]       items
+        )
     {
         ArrayIterator<T> iter = new ArrayIterator<T>(clazz, items[0], items[1]);
         test_BuildFromItems( clazz, iter, 2 );
@@ -124,7 +136,11 @@ public class ArrayIteratorTest
         test_BuildFromItems( clazz, iter, 3 );
     }
 
-    public <T> void test_BuildFromItems( final Class<T> clazz, final Iterator<T> iter, final int xCount)
+    private static <T> void test_BuildFromItems(
+        final Class<T>      clazz,
+        final Iterator<T>   iter,
+        final int           xCount
+        )
     {
         int count = 0;
 
