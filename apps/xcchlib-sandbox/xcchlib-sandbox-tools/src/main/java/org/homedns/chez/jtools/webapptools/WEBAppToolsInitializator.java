f8f355573ce42c95b72d03a170b5b1e7409acc36
CClaude <Claude.CHOISNET@gmail.com>
2016-12-25 10:51:47 +0100

    Cleaning

--- apps/xcchlib-sandbox/xcchlib-sandbox-tools/src/main/java/org/homedns/chez/jtools/webapptools/WEBAppToolsInitializator.java
+++ apps/xcchlib-sandbox/xcchlib-sandbox-tools/src/main/java/org/homedns/chez/jtools/webapptools/WEBAppToolsInitializator.java
@@ -1,147 +1,96 @@
-/*
-** -----------------------------------------------------------------------
-** Nom           : org/homedns/chez/jtools/webapptools/WEBAppToolsInitializator.java
-** Description   :
-** Encodage      : ANSI
-**
-**  1.01.001 2007.01.01 Claude CHOISNET - Version initiale
-** -----------------------------------------------------------------------
-**
-** org.homedns.chez.jtools.webapptools.WEBAppToolsInitializator
-**
-*/
 package org.homedns.chez.jtools.webapptools;
 
-import javax.servlet.http.HttpServletRequest;
 import javax.servlet.ServletContext;
+import javax.servlet.http.HttpServletRequest;
 import org.homedns.chez.jtools.lib.ContextInitializable;
 import org.homedns.chez.jtools.lib.ContextLoader;
 
 /**
-**
-** @author Claude CHOISNET
-** @since   1.01.001
-** @version 1.01.001
-*/
-public class WEBAppToolsInitializator
-    implements ContextInitializable<WEBAppToolsContext>
+ *
+ * @since 1.01
+ */
+public class WEBAppToolsInitializator implements ContextInitializable<WEBAppToolsContext>
 {
+    /**
+     * Nom du contexte pour l'application WEB
+     *
+     * @see #getServletContextName()
+     * @see #newInstance(HttpServletRequest)
+     */
+    public static final String SERVLET_CONTEXT_NAME = "/Tools";
 
-/**
-** <p>Nom du contexte pour l'application WEB</p>
-**
-** @see #getServletContextName()
-** @see #newInstance(HttpServletRequest)
-*/
-public static final String SERVLET_CONTEXT_NAME = "/Tools" ;
+    /**
+     * Nom de l'objet {@link WEBAppToolsContext} dans les attributs du context de servlet
+     *
+     * @see #getAttributeName()
+     * @see #newInstance(HttpServletRequest)
+     */
+    public static final String ATTRIBUTE_NAME
+        = WEBAppToolsContext.class.getName();
 
-/**
-** <p>Nom de l'objet {@link WEBAppToolsContext} dans les attributs du context de servlet</p>
-**
-** @see #getAttributeName()
-** @see #newInstance(HttpServletRequest)
-*/
-public static final String ATTRIBUTE_NAME
-        = "org.homedns.chez.jtools.webapptools.WEBAppToolsContext";
+    public static final String SEPARATOR_REGEXP
+        = "[\t\n\r ,;]+";
 
-/**
-**
-*/
-public static final String SEPARATOR_REGEXP = "[\t\n\r ,;]+";
+    public static final String PROXY_VIEW_URLS_PARAM_NAME = "proxyViewURLs";
+    public static final String HOST_LIST_PARAM_NAME       = "hostList";
 
-/**
-**
-*/
-public static final String PROXY_VIEW_URLS_PARAM_NAME = "proxyViewURLs";
+    protected WEBAppToolsInitializator()
+    {
+        // empty, just for somes init use !
+    }
 
-/**
-**
-*/
-public static final String HOST_LIST_PARAM_NAME = "hostList";
+    @Override
+    public String getServletContextName()
+    {
+        return SERVLET_CONTEXT_NAME;
+    }
 
-/**
-**
-*/
-protected WEBAppToolsInitializator() // -------------------------------------
-{
- // empty, just for somes init use !
-}
+    @Override
+    public String getAttributeName()
+    {
+        return ATTRIBUTE_NAME;
+    }
 
-/**
-**
-*/
-@Override
-public String getServletContextName() // ----------------------------------
-{
- return SERVLET_CONTEXT_NAME;
-}
+    public static String getServletContextInitParameter(
+        final ServletContext servletContext,
+        final String         ctxtName
+        ) throws WEBAppToolsInitializationException
+    {
+        try {
+            final String value = servletContext.getInitParameter( ctxtName ).trim();
 
-/**
-**
-*/
-@Override
-public String getAttributeName() // ---------------------------------------
-{
- return ATTRIBUTE_NAME;
-}
+            if( value.length() > 0 ) {
+                return value;
+            }
 
-/**
-**
-*/
-static
-public String getServletContextInitParameter( // --------------------------
-    final ServletContext    servletContext,
-    final String            ctxtName
-    )
-    throws WEBAppToolsInitializationException
-{
- try {
-    String value = servletContext.getInitParameter( ctxtName ).trim();
+            throw new WEBAppToolsInitializationException( "Value \"" + ctxtName + "\" is empty" );
+        }
+        catch( final Exception e ) {
+            throw new WEBAppToolsInitializationException( "Value \"" + ctxtName + "\" not found in context (web.xml:" + servletContext + ")", e );
+        }
+    }
 
-    if( value.length() > 0 ) {
-//        getLogger().info( "Ctxt \"" + ctxtName + "\" = \"" + value + "\"" );
+    @Override
+    public WEBAppToolsContext newInstance( final HttpServletRequest request )
+        throws WEBAppToolsInitializationException
+    {
+        final ServletContext sc = ContextLoader.getServletContext( request, this );
+        final WEBAppToolsContext ctxt = new WEBAppToolsContext();
 
-        return value;
+        String   value  = getServletContextInitParameter( sc, PROXY_VIEW_URLS_PARAM_NAME );
+        String[] values = value.split( SEPARATOR_REGEXP );
+
+        for( final String v : values ) {
+            ctxt.addProxyViewURL( v );
         }
 
-    throw new WEBAppToolsInitializationException( "Value \"" + ctxtName + "\" is empty" );
-    }
- catch( Exception e ) {
-//    getLogger().debug( "getServletContextInitParameter(): servletContext = " + servletContext, e );
+        value  = getServletContextInitParameter( sc, HOST_LIST_PARAM_NAME );
+        values = value.split( SEPARATOR_REGEXP );
 
-    throw new WEBAppToolsInitializationException(
-        "Value \"" + ctxtName + "\" not found in context (web.xml:" + servletContext + ")",
-        e
-        );
+        for( final String v : values ) {
+            ctxt.addHostName( v );
+        }
+
+        return ctxt;
     }
 }
-
-
-/**
-**
-*/
-@Override
-public WEBAppToolsContext newInstance( HttpServletRequest request ) // ----
-    throws WEBAppToolsInitializationException
-{
- final ServletContext       sc      = ContextLoader.getServletContext( request, this );
- final WEBAppToolsContext   ctxt    = new WEBAppToolsContext();
-
- String     value   = getServletContextInitParameter( sc, PROXY_VIEW_URLS_PARAM_NAME );
- String[]   values  = value.split( SEPARATOR_REGEXP );
-
- for( String v : values ) {
-    ctxt.addProxyViewURL( v );
-    }
-
- value   = getServletContextInitParameter( sc, HOST_LIST_PARAM_NAME );
- values  = value.split( SEPARATOR_REGEXP );
-
- for( String v : values ) {
-    ctxt.addHostName( v );
-    }
-
- return ctxt;
-}
-
-} // class
