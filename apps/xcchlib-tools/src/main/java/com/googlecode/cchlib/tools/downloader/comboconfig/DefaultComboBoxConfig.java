811a208e79df429a6a571cd90a3ae829d4b12593
CClaude <Claude.CHOISNET@gmail.com>
2016-12-26 13:28:09 +0100

    Fix missing serial number

--- apps/xcchlib-tools/src/main/java/com/googlecode/cchlib/tools/downloader/comboconfig/DefaultComboBoxConfig.java
+++ apps/xcchlib-tools/src/main/java/com/googlecode/cchlib/tools/downloader/comboconfig/DefaultComboBoxConfig.java
@@ -14,6 +14,8 @@ public class DefaultComboBoxConfig
     @SuppressWarnings("squid:S2176")
     private static class Item implements GenericDownloaderUIPanelEntry.Item
     {
+        private static final long serialVersionUID = 1L;
+
         private final String jComboBoxText;
         private final String selectedDescription;
 
