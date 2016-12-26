0a9b493e048347924e3ecda80f5ff37141e4b88e
CClaude <Claude.CHOISNET@gmail.com>
2016-12-26 20:23:00 +0100

    Cleaning

--- apps/xcchlib-sandbox/xcchlib-sandbox-lib/src/main/java/jrpdk/io/swing/OutputStreamInJTextAreaAndLogin.java
+++ apps/xcchlib-sandbox/xcchlib-sandbox-lib/src/main/java/jrpdk/io/swing/OutputStreamInJTextAreaAndLogin.java
@@ -1,125 +1,119 @@
-/*
-** $VER: OutputStreamInJTextAreaAndLogin.java
-*/
 package jrpdk.io.swing;
 
+import java.io.IOException;
 import java.io.OutputStream;
 import javax.swing.JTextArea;
-import java.io.IOException;
 
 /**
-** Objet de type OutputStream dont les donnees sont redirigees vers
-** une sortie graphique ainsi que vers un autre flux de type OutputStream
-**
-** @author Claude CHOISNET
-** @version 1.00 09/01/2002
-*/
+ * Objet de type OutputStream dont les donnees sont redirigees vers une
+ * sortie graphique ainsi que vers un autre flux de type OutputStream
+ *
+ * @since 1.00
+ */
 public class OutputStreamInJTextAreaAndLogin extends OutputStream
 {
-private JTextArea       jTextArea;
-private StringBuffer    textBuffer;
-private OutputStream    loginStream;
+    private final JTextArea    jTextArea;
+    private final OutputStream loginStream;
+    private final StringBuffer textBuffer;
 
-/**
-**
-*/
-public OutputStreamInJTextAreaAndLogin( // --------------------------------
-    JTextArea       jTextArea,
-    OutputStream    loginStream
-    )
-{
- super();
+    public OutputStreamInJTextAreaAndLogin(
+        final JTextArea    jTextArea,
+        final OutputStream loginStream
+        )
+    {
+        super();
 
- this.jTextArea     = jTextArea;
- this.textBuffer    = new StringBuffer();
- this.loginStream   = loginStream;
-}
-
-/**
-** Closes this output stream and releases any system resources
-** associated with this stream.
-*/
-@Override
-public void close() throws IOException // ---------------------------------
-{
- flush();
-
- loginStream.close();
-}
-
-/**
-** Flushes this output stream and forces any buffered output
-** bytes to be written out.
-*/
-@Override
-public void flush() throws IOException // ---------------------------------
-{
- flushBuffer();
-
- loginStream.flush();
-}
-
-/**
-** Writes b.length bytes from the specified byte array to this
-** output stream.
-*/
-@Override
-public void write( byte[] b ) throws IOException // -----------------------
-{
- writeBuffer( new String( b ) );
-
- loginStream.write( b );
-}
-
-/**
-** Writes len bytes from the specified byte array starting at
-** offset off to this output stream.
-*/
-@Override
-public void write( byte[] b, int off, int len ) throws IOException //------
-{
- writeBuffer( new String( b, off, len ) );
-
- loginStream.write( b, off, len );
-}
-
-/**
-** Writes the specified byte to this output stream.
-*/
-@Override
-public void write( int b ) throws IOException // --------------------------
-{
- StringBuffer   sb = new StringBuffer();
-
- sb.append( (char)b );
-
- writeBuffer( sb.toString() );
-
- loginStream.write( b );
-}
-
-/**
-**
-*/
-final private void writeBuffer( String str ) throws IOException // --------
-{
- textBuffer.append( str );
- flushBuffer();
-}
-
-/**
-**
-*/
-final private void flushBuffer() // ---------------------------------------
-{
- String s = textBuffer.toString();
-
- if( s.length() > 0 ) {
-    jTextArea.append( s );
-
-    textBuffer = new StringBuffer();
+        this.jTextArea   = jTextArea;
+        this.textBuffer  = new StringBuffer();
+        this.loginStream = loginStream;
     }
- // else le buffer est vide !
-}
 
-} // class
+    /**
+     * {@inheritDoc}
+     *
+     * Closes this output stream and releases any system resources
+     * associated with this stream.
+     */
+    @Override
+    public void close() throws IOException
+    {
+        flush();
+
+        this.loginStream.close();
+    }
+
+    /**
+     * {@inheritDoc}
+     *
+     * Flushes this output stream and forces any buffered output bytes to be written out.
+     */
+    @Override
+    public void flush() throws IOException
+    {
+        flushBuffer();
+
+        this.loginStream.flush();
+    }
+
+    /**
+     * {@inheritDoc}
+     *
+     * Writes b.length bytes from the specified byte array to this output stream.
+     */
+    @Override
+    public void write( final byte[] b ) throws IOException
+    {
+        writeBuffer( new String( b ) );
+
+        this.loginStream.write( b );
+    }
+
+    /**
+     * {@inheritDoc}
+     *
+     * Writes len bytes from the specified byte array starting at offset off
+     * to this output stream.
+     */
+    @Override
+    public void write( final byte[] b, final int off, final int len ) throws IOException
+    {
+        writeBuffer( new String( b, off, len ) );
+
+        this.loginStream.write( b, off, len );
+    }
+
+    /**
+     * {@inheritDoc}
+     *
+     * Writes the specified byte to this output stream.
+     */
+    @Override
+    public void write( final int b ) throws IOException
+    {
+        final StringBuilder sb = new StringBuilder();
+
+        sb.append( (char)b );
+
+        writeBuffer( sb.toString() );
+
+        this.loginStream.write( b );
+    }
+
+    private final void writeBuffer( final String str ) throws IOException
+    {
+        this.textBuffer.append( str );
+
+        flushBuffer();
+    }
+
+    private final void flushBuffer()
+    {
+        final String s = this.textBuffer.toString();
+
+        if( s.length() > 0 ) {
+            this.jTextArea.append( s );
+            this.textBuffer.setLength( 0 );
+        }
+        // else le buffer est vide !
+    }
+}
