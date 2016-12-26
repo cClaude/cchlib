206141dcf7b463d741f05c7f4424f02c70d2f6e9
CClaude <Claude.CHOISNET@gmail.com>
2016-12-26 14:27:35 +0100

    Small refactoring

--- apps/xcchlib-core-sample/src/main/java/paper/reflexion/serialization/ParentAccessSerializable.java
+++ apps/xcchlib-core-sample/src/main/java/paper/reflexion/serialization/ParentAccessSerializable.java
@@ -8,26 +8,23 @@ import java.lang.reflect.Field;
 import java.lang.reflect.Method;
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Collections;
 import java.util.List;
 import paper.reflexion.ParentAccess;
 
-/**
- *
- */
 public class ParentAccessSerializable<C>
     extends ParentAccess<C>
         implements Serializable
 {
     private static final long serialVersionUID = 1L;
-    private transient List<Field> visibleFields;
-    private transient List<Field> hiddenFields;
+
+    private transient List<Field>  visibleFields;
+    private transient List<Field>  hiddenFields;
+
     private transient List<Method> visibleMethods;
     private transient List<Method> hiddenMethods;
 
-    /**
-     *
-     */
-    public ParentAccessSerializable( Class<C> clazz )
+    public ParentAccessSerializable( final Class<C> clazz )
     {
         super( clazz );
 
@@ -36,36 +33,86 @@ public class ParentAccessSerializable<C>
 
     private void initTransientFields()
     {
-        this.visibleFields  = Arrays.asList( super.getEnclosingClass().getFields() );
-        this.hiddenFields   = new ArrayList<Field>();
+        this.visibleFields  = createVisibleFields(
+                super.getEnclosingClass().getFields()
+                );
+        this.hiddenFields   = createHiddenFields(
+                super.getEnclosingClass().getDeclaredFields(),
+                this.visibleFields
+                );
 
-        for( Field f : super.getEnclosingClass().getDeclaredFields() ) {
-            if( ! visibleFields.contains( f ) ) {
-                this.hiddenFields.add( f );
-                }
-            }
-
-        this.visibleMethods = Arrays.asList( super.getEnclosingClass().getMethods() );
-        this.hiddenMethods  = new ArrayList<Method>();
-
-        for( Method m : super.getEnclosingClass().getDeclaredMethods() ) {
-            if( !visibleMethods.contains( m ) ) {
-                this.hiddenMethods.add( m );
-                }
-            }
+        this.visibleMethods = createVisibleMethods(
+                super.getEnclosingClass().getMethods()
+                );
+        this.hiddenMethods  = createHiddenMethods(
+                super.getEnclosingClass().getDeclaredMethods(),
+                this.visibleMethods
+                );
     }
 
-    public List<Field> getHiddenFieldList()
+    private static List<Field> createVisibleFields( final Field[] fields )
+    {
+        return Collections.unmodifiableList( Arrays.asList( fields ) );
+    }
+
+    private static List<Field> createHiddenFields(
+        final Field[]     declaredFields,
+        final List<Field> visibleFields
+        )
+    {
+        final ArrayList<Field> hiddenFields = new ArrayList<>();
+
+        for( final Field field : declaredFields ) {
+            if( ! visibleFields.contains( field ) ) {
+                hiddenFields.add( field );
+                }
+            }
+
+        return Collections.unmodifiableList( hiddenFields );
+    }
+
+    private static List<Method> createVisibleMethods( final Method[] methods )
+    {
+        return Collections.unmodifiableList( Arrays.asList( methods ) );
+    }
+
+    private static List<Method> createHiddenMethods(
+        final Method[]     declaredMethods,
+        final List<Method> visibleMethods
+        )
+    {
+        final List<Method> hiddenMethods  = new ArrayList<>();
+
+        for( final Method m : declaredMethods ) {
+            if( ! visibleMethods.contains( m ) ) {
+                hiddenMethods.add( m );
+                }
+            }
+
+        return Collections.unmodifiableList( hiddenMethods );
+    }
+
+    public List<Field> getVisibleFields()
+    {
+        return this.visibleFields;
+    }
+
+    public List<Field> getHiddenFields()
     {
         return this.hiddenFields;
     }
 
-    public List<Method> getHiddenMethodList()
+    public List<Method> getVisibleMethods()
+    {
+        return this.visibleMethods;
+    }
+
+    public List<Method> getHiddenMethods()
     {
         return this.hiddenMethods;
     }
 
-    private void writeObject( ObjectOutputStream out ) throws IOException
+    private void writeObject( final ObjectOutputStream out ) throws IOException
     {
         // Default serialization process (store only 'clazz' field (on parent class) )
         out.defaultWriteObject();
@@ -74,7 +121,7 @@ public class ParentAccessSerializable<C>
         // method could be removed in this case
     }
 
-    private void readObject( ObjectInputStream in ) throws IOException, ClassNotFoundException
+    private void readObject( final ObjectInputStream in ) throws IOException, ClassNotFoundException
     {
         // Default serialization process  (restore only 'clazz' (on parent class) field)
         in.defaultReadObject();
--- apps/xcchlib-core-sample/src/test/java/paper/reflexion/serialization/ParentAccessSerializableTest.java
+++ apps/xcchlib-core-sample/src/test/java/paper/reflexion/serialization/ParentAccessSerializableTest.java
@@ -6,34 +6,30 @@ import java.util.Iterator;
 import java.util.List;
 import org.junit.Assert;
 import org.junit.Test;
-import com.googlecode.cchlib.test.SerializableTestCaseHelper;
 import paper.reflexion.LegacyParentClass;
-import paper.reflexion.serialization.ParentAccessSerializable;
+import com.googlecode.cchlib.test.SerializableTestCaseHelper;
 
-/**
- *
- */
 public class ParentAccessSerializableTest
 {
     @Test
     public void test_Serialization() throws ClassNotFoundException, IOException
     {
-        ParentAccessSerializable<LegacyParentClass> pa0 = new ParentAccessSerializable<LegacyParentClass>( LegacyParentClass.class );
+        final ParentAccessSerializable<LegacyParentClass> pa0 = new ParentAccessSerializable<LegacyParentClass>( LegacyParentClass.class );
 
         // Launch serialization
-        ParentAccessSerializable<LegacyParentClass> pa1 = SerializableTestCaseHelper.cloneOverSerialization( pa0 );
+        final ParentAccessSerializable<LegacyParentClass> pa1 = SerializableTestCaseHelper.cloneOverSerialization( pa0 );
 
-        List<Field> hiddenFields0 = pa0.getHiddenFieldList();
-        List<Field> hiddenFields1 = pa1.getHiddenFieldList();
+        final List<Field> hiddenFields0 = pa0.getHiddenFields();
+        final List<Field> hiddenFields1 = pa1.getHiddenFields();
 
         Assert.assertEquals( hiddenFields0.size(), hiddenFields1.size() );
 
-        Iterator<Field> i0 = hiddenFields0.iterator();
-        Iterator<Field> i1 = hiddenFields1.iterator();
+        final Iterator<Field> i0 = hiddenFields0.iterator();
+        final Iterator<Field> i1 = hiddenFields1.iterator();
 
         while( i0.hasNext() && i1.hasNext() ) {
-            Field v0 = i0.next();
-            Field v1 = i1.next();
+            final Field v0 = i0.next();
+            final Field v1 = i1.next();
 
             Assert.assertEquals( v0, v1 );
             }
