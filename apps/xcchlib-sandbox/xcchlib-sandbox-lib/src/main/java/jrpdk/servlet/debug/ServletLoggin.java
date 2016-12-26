75297e7eae1f4ea0f3a5cc0af71f5e86e2985cc4
CClaude <Claude.CHOISNET@gmail.com>
2016-12-26 18:50:26 +0100

    Cleaning + doc

--- apps/xcchlib-sandbox/xcchlib-sandbox-lib/src/main/java/jrpdk/servlet/debug/ServletLoggin.java
+++ apps/xcchlib-sandbox/xcchlib-sandbox-lib/src/main/java/jrpdk/servlet/debug/ServletLoggin.java
@@ -1,77 +1,52 @@
-/*
-** $VER: ServletLoggin.java
-*/
 package jrpdk.servlet.debug;
 
-//import commun.util.mail.SendAnException;
 import javax.servlet.http.HttpServletRequest;
-//import commun.util.mail.ProjectID;
 
 /*
-** Classe permettant d'armoniser la gestion des traces pour les
-** erreurs.
-** <P>
-** Cette doit étre utilisée pour récupérer des exceptions qui sont
-** typiquement des bugs (problémes d'algoritmique, ...).
-**
-**
-** @author Claude CHOISNET
-** @version 1.00 22/11/2000
-**
-*/
+ * Classe permettant d'armoniser la gestion des traces pour les
+ * erreurs.
+ * <P>
+ * Cette doit étre utilisée pour récupérer des exceptions qui sont
+ * typiquement des bugs (problémes d'algoritmique, ...).
+ */
 public class ServletLoggin
 {
+    private ServletLoggin()
+    {
+        // Static
+    }
 
-/**
-**
-*/
-public static void trace( // ----------------------------------------------
-    Object      object,
-    Exception   exception,
-    String      message
-    )
-{
- trace( object, exception, null, message );
+    public static void trace(
+        final Object    object,
+        final Exception exception,
+        final String    message
+        )
+    {
+        trace( object, exception, null, message );
+    }
+
+    public static void trace(
+        final Object             object,
+        final Exception          exception,
+        final HttpServletRequest request,
+        final String             message
+        )
+    {
+        // Fichier de log du serveur
+        // System.err.println( "***$$HTMLBuilderDebugger : " + exception );
+        // exception.printStackTrace( System.err );
+
+        TraceToFile.println( "object:" + object );
+        TraceToFile.println( "exception:" + exception );
+        TraceToFile.println( "request:" + request );
+        TraceToFile.println( "Message:" + message );
+
+        /*// Emission d'un mail SendAnException.sendAnException( ProjectID.TOOLS_HTML_BUILDER, object, exception,
+         * request, message ); */
+    }
+
+    public static void println( final String str )
+    {
+        TraceToFile.println( str );
+    }
 }
-
-/**
-**
-*/
-public static void trace( // ----------------------------------------------
-    Object              object,
-    Exception           exception,
-    HttpServletRequest  request,
-    String              message
-    )
-{
- // Fichier de log du serveur
-// System.err.println( "***$$HTMLBuilderDebugger : " + exception );
-// exception.printStackTrace( System.err );
-
- TraceToFile.println( "object:" + object );
- TraceToFile.println( "exception:" + exception );
- TraceToFile.println( "request:" + request );
- TraceToFile.println( "Message:" + message );
-
-/*
- // Emission d'un mail
- SendAnException.sendAnException(
-    ProjectID.TOOLS_HTML_BUILDER,
-    object,
-    exception,
-    request,
-    message
-    );
-*/
-}
-
-/**
-**
-*/
-public static void println( String s ) // ---------------------------------
-{
- TraceToFile.println( s );
-}
-
-
-} // class
