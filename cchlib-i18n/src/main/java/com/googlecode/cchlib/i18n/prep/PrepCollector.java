a6548c065ef73396c63dbc5d18523ad95f764c8f
CClaude <Claude.CHOISNET@gmail.com>
2017-01-02 17:39:24 +0100

    Add isEmpty()

--- cchlib-i18n/src/main/java/com/googlecode/cchlib/i18n/prep/PrepCollector.java
+++ cchlib-i18n/src/main/java/com/googlecode/cchlib/i18n/prep/PrepCollector.java
@@ -2,35 +2,38 @@ package com.googlecode.cchlib.i18n.prep;
 
 import java.util.ArrayList;
 import java.util.Collections;
-import java.util.Comparator;
 import java.util.HashMap;
 import java.util.Iterator;
 import java.util.Map;
 import java.util.Set;
 
-public class PrepCollector<T> implements Iterable<Map.Entry<String,T>>
+public final class PrepCollector<T> implements Iterable<Map.Entry<String,T>>
 {
     private final Map<String,T> map = new HashMap<>();
 
     public void add( final String key, final T value )
     {
-        map.put( key, value );
+        this.map.put( key, value );
     }
 
     @Override
     public Iterator<Map.Entry<String,T>> iterator()
     {
-        final Set<Map.Entry<String,T>>       entrySet = map.entrySet();
+        final Set<Map.Entry<String,T>>       entrySet = this.map.entrySet();
         final ArrayList<Map.Entry<String,T>> list     = new ArrayList<>( entrySet );
 
-        Collections.sort( list, new Comparator<Map.Entry<String,T>>() {
-            @Override
-            public int compare( final Map.Entry<String,T> o1, final Map.Entry<String,T> o2 )
-            {
-                return o1.getKey().compareTo( o2.getKey() );
-            }
-        });
+        Collections.sort( list, this::compare );
 
         return Collections.unmodifiableList( list ).iterator();
+    }
+
+    private int compare( final Map.Entry<String, T> o1, final Map.Entry<String, T> o2 )
+    {
+        return o1.getKey().compareTo( o2.getKey() );
+    }
+
+    public boolean isEmpty()
+    {
+        return this.map.isEmpty();
     }
 }
