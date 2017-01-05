0e280a7d6f51d189a143450b1f56afb080d00382
CClaude <Claude.CHOISNET@gmail.com>
2017-01-05 19:41:00 +0100

    Add javadoc (and sort Unused properties by key)

--- cchlib-i18n/src/main/java/com/googlecode/cchlib/i18n/resourcebuilder/I18nResourceBuilderHelper.java
+++ cchlib-i18n/src/main/java/com/googlecode/cchlib/i18n/resourcebuilder/I18nResourceBuilderHelper.java
@@ -4,12 +4,59 @@ import java.io.File;
 import java.io.IOException;
 import java.io.PrintStream;
 import java.util.Map;
+import java.util.stream.Stream;
 import com.googlecode.cchlib.i18n.resources.I18nResourceBundleName;
 import com.googlecode.cchlib.i18n.resources.I18nResourceBundleNameFactory;
 
 @SuppressWarnings("ucd") // API
 public class I18nResourceBuilderHelper
 {
+    private static final class I18nResourceBuilderResultFormat
+    {
+        private final PrintStream ps;
+        private final String      header;
+        private final String      linePrefix;
+        private final String      separator;
+
+        I18nResourceBuilderResultFormat(
+            final PrintStream ps,
+            final String      header,
+            final String      linePrefix,
+            final String      separator
+            )
+        {
+            this.ps           = ps;
+            this.header       = header;
+            this.linePrefix   = linePrefix;
+            this.separator    = separator;
+        }
+
+        public void print( final Map<String,Integer> keysUsageCount )
+        {
+            this.ps.println( this.header );
+
+            keysUsageCount.entrySet().stream().forEachOrdered( this::print );
+
+            this.ps.println( '#' );
+            this.ps.flush();
+        }
+
+        public void print( final Stream<Map.Entry<String,String>> keysValuesStream )
+        {
+            this.ps.println( this.header );
+
+            keysValuesStream.forEachOrdered( this::print );
+
+            this.ps.println( '#' );
+            this.ps.flush();
+        }
+
+        private void print( final Map.Entry<String,?> e )
+        {
+            this.ps.println( this.linePrefix + e.getKey() + this.separator + e.getValue() );
+        }
+   }
+
     private I18nResourceBuilderHelper()
     {
         // All static
@@ -63,6 +110,17 @@ public class I18nResourceBuilderHelper
         return new File( getCurrentDirectoryCanonicalFile(), filenamePrefix + ".properties" );
     }
 
+    /**
+     * Just an alias that call {@link #fmtLocalizedFields(PrintStream, I18nResourceBuilderResult)},
+     * then {@link #fmtIgnoredFields(PrintStream, I18nResourceBuilderResult)},
+     * and then {@link #fmtMissingProperties(PrintStream, I18nResourceBuilderResult)}
+     * and finally {@link #fmtUnusedProperties(PrintStream, I18nResourceBuilderResult)}
+     *
+     * @param ps
+     *            {@link PrintStream} for output.
+     * @param result
+     *            Value from {@link I18nResourceBuilder#getResult()}
+     */
     public static void fmtAll(
         final PrintStream               ps,
         final I18nResourceBuilderResult result
@@ -71,73 +129,101 @@ public class I18nResourceBuilderHelper
         fmtLocalizedFields( ps, result );
         fmtIgnoredFields( ps, result );
         fmtMissingProperties( ps, result );
+
         fmtUnusedProperties( ps, result );
     }
 
+    /**
+     * Format to {@code ps} ordered values from
+     * {@link I18nResourceBuilderResult#getLocalizedFields()}
+     * (discover order)
+     *
+     * @param ps
+     *            {@link PrintStream} for output.
+     * @param result
+     *            Value from {@link I18nResourceBuilder#getResult()}
+     */
     public static void fmtLocalizedFields(
         final PrintStream               ps,
         final I18nResourceBuilderResult result
         )
     {
-        ps.println( "# localizedFieldMap (key:usageCount) " );
-
-        fmtData( ps, "# localized : ", " : ", result.getLocalizedFields() );
-
-        ps.println( '#' );
+        new I18nResourceBuilderResultFormat(
+                ps,
+                "# localizedFieldMap (key:usageCount)",
+                "# localized : ",
+                " : "
+                ).print( result.getLocalizedFields() );
     }
 
+    /**
+     * Format to {@code ps} ordered values from
+     * {@link I18nResourceBuilderResult#getIgnoredFields()}
+     * (discover order)
+     *
+     * @param ps
+     *            {@link PrintStream} for output.
+     * @param result
+     *            Value from {@link I18nResourceBuilder#getResult()}
+     */
     public static void fmtIgnoredFields(
         final PrintStream               ps,
         final I18nResourceBuilderResult result
         )
     {
-        ps.println( "# ignoredFields (key:usageCount) " );
-
-        fmtData( ps, "# ignored : ", " : ", result.getIgnoredFields() );
-
-        ps.println( '#' );
+        new I18nResourceBuilderResultFormat(
+                ps,
+                "# ignoredFields (key:usageCount)",
+                "# ignored : ",
+                " : " ).print( result.getIgnoredFields() );
     }
 
+    /**
+     * Format to {@code ps} ordered values from
+     * {@link I18nResourceBuilderResult#getMissingProperties()}
+     * (discover order)
+     *
+     * @param ps
+     *            {@link PrintStream} for output.
+     * @param result
+     *            Value from {@link I18nResourceBuilder#getResult()}
+     */
     public static void fmtMissingProperties(
         final PrintStream               ps,
         final I18nResourceBuilderResult result
         )
     {
-        ps.println( "# missingProperties (key:usageCount) " );
-
-        fmtData( ps, "", " : ", result.getMissingProperties() );
-
-        ps.println( '#' );
-        ps.flush();
+        new I18nResourceBuilderResultFormat(
+                ps,
+                "# missingProperties (key:usageCount)",
+                "",
+                " : "
+                ).print( result.getMissingProperties() );
     }
 
-    public static void fmtUnusedProperties( final PrintStream ps, final I18nResourceBuilderResult result )
-    {
-        fmtKeyValue( ps, "unusedProperties", result.getUnusedProperties() );
-    }
-
-    private static void fmtKeyValue(
-        final PrintStream        ps ,
-        final String             name,
-        final Map<String,String> keysValues
+    /**
+     * Format to {@code ps} ordered values from
+     * {@link I18nResourceBuilderResult#getUnusedProperties()}
+     * (natural order on key)
+     *
+     * @param ps
+     *            {@link PrintStream} for output.
+     * @param result
+     *            Value from {@link I18nResourceBuilder#getResult()}
+     */
+    public static void fmtUnusedProperties(
+        final PrintStream               ps,
+        final I18nResourceBuilderResult result
         )
     {
-        ps.println( "# " + name + " (key=value) " );
-
-        fmtData( ps, "", "=", keysValues );
-
-        ps.println( '#' );
-    }
-
-    private static void fmtData(
-        final PrintStream   ps ,
-        final String        linePrefix,
-        final String        separator,
-        final Map<String,?> keyValues
-        )
-    {
-        for( final Map.Entry<String,?> entry : keyValues.entrySet() ) {
-            ps.println( linePrefix + entry.getKey() + separator + entry.getValue() );
-        }
+        new I18nResourceBuilderResultFormat(
+                ps,
+                "# unusedProperties (key=value)",
+                "unused : ",
+                "=" ).print(
+                        result.getUnusedProperties().entrySet().stream().sorted(
+                                (e1,e2) -> e1.getKey().compareTo( e2.getKey() )
+                                )
+                        );
     }
 }
