769a4cc33241d41fa0214c64c1152ab359e161f7
CClaude <Claude.CHOISNET@gmail.com>
2017-01-06 22:12:25 +0100

    Add comment

--- cchlib-i18n-test/src/test/java/com/googlecode/cchlib/i18n/unit/TestReference.java
+++ cchlib-i18n-test/src/test/java/com/googlecode/cchlib/i18n/unit/TestReference.java
@@ -3,6 +3,10 @@ package com.googlecode.cchlib.i18n.unit;
 import com.googlecode.cchlib.i18n.core.I18nAutoCoreUpdatable;
 import com.googlecode.cchlib.i18n.resourcebuilder.I18nResourceBuilderResult;
 
+/**
+ * extends "I18nAutoCoreUpdatable" is required by tests and I18nResourceBuilder
+ * process, but not formally by I18n process
+ */
 public interface TestReference extends I18nAutoCoreUpdatable
 {
     void beforePerformeI18nTest();
