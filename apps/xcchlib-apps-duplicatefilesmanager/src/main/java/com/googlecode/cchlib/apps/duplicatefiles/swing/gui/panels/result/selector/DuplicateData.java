cb70930bf06a5c03b1f0b3dbc3a71fccf6f502be
CClaude <Claude.CHOISNET@gmail.com>
2017-01-11 17:04:31 +0100

    Let know IDE that class is Serializable

--- apps/xcchlib-apps-duplicatefilesmanager/src/main/java/com/googlecode/cchlib/apps/duplicatefiles/swing/gui/panels/result/selector/DuplicateData.java
+++ apps/xcchlib-apps-duplicatefilesmanager/src/main/java/com/googlecode/cchlib/apps/duplicatefiles/swing/gui/panels/result/selector/DuplicateData.java
@@ -1,9 +1,11 @@
 package com.googlecode.cchlib.apps.duplicatefiles.swing.gui.panels.result.selector;
 
+import java.io.Serializable;
 import com.googlecode.cchlib.apps.duplicatefiles.swing.gui.panels.result.JPanelResultListModel;
 
-public interface DuplicateData
+public interface DuplicateData extends Serializable
 {
     JPanelResultListModel getListModelDuplicatesFiles();
+
     void updateDisplay();
 }
