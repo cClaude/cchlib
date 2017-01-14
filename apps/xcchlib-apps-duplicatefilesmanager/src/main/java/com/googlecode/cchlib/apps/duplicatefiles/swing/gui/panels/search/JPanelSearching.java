0bab44b3bcfa5213f3038bdb60d9aa9a54a329a7
CClaude <Claude.CHOISNET@gmail.com>
2017-01-14 16:44:37 +0100

    Sonar

--- apps/xcchlib-apps-duplicatefilesmanager/src/main/java/com/googlecode/cchlib/apps/duplicatefiles/swing/gui/panels/search/JPanelSearching.java
+++ apps/xcchlib-apps-duplicatefilesmanager/src/main/java/com/googlecode/cchlib/apps/duplicatefiles/swing/gui/panels/search/JPanelSearching.java
@@ -1,15 +1,12 @@
 package com.googlecode.cchlib.apps.duplicatefiles.swing.gui.panels.search;
 
 
+
 @SuppressWarnings("squid:MaximumInheritanceDepth")
 public abstract class JPanelSearching
     extends JPanelSearchingFilters
         implements Cancelable
 {
-    enum Pass {
-        PASS1, PASS2,
-    }
-
     private static final long serialVersionUID = 1L;
 
     protected JPanelSearching( final int nThreads )
