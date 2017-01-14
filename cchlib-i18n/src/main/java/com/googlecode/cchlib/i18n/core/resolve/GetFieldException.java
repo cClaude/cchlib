01f2e8cd84b9ee642d468080a604c5cbbab5753b
CClaude <Claude.CHOISNET@gmail.com>
2017-01-14 23:07:12 +0100

    Cleaning

--- cchlib-i18n/src/main/java/com/googlecode/cchlib/i18n/core/resolve/GetFieldException.java
+++ cchlib-i18n/src/main/java/com/googlecode/cchlib/i18n/core/resolve/GetFieldException.java
@@ -1,15 +1,15 @@
 package com.googlecode.cchlib.i18n.core.resolve;
 
-public class GetFieldException extends I18nFieldException 
+public class GetFieldException extends I18nFieldException
 {
     private static final long serialVersionUID = 1L;
 
-    public GetFieldException( String message, Throwable cause )
+    public GetFieldException( final String message, final Throwable cause )
     {
         super( message, cause );
     }
-    
-    public GetFieldException( Throwable cause )
+
+    public GetFieldException( final Throwable cause )
     {
         super( cause );
     }
