7d9c053f5b51aa1179a893515e125aa9b7b5111a
CClaude <Claude.CHOISNET@gmail.com>
2017-01-14 17:33:07 +0100

    Sonar

--- cchlib-swing/src/main/java/cx/ath/choisnet/swing/introspection/FramePopulator.java
+++ cchlib-swing/src/main/java/cx/ath/choisnet/swing/introspection/FramePopulator.java
@@ -2,22 +2,28 @@ package cx.ath.choisnet.swing.introspection;
 
 import cx.ath.choisnet.lang.introspection.IntrospectionInvokeException;
 
+@FunctionalInterface
+@SuppressWarnings("squid:S00119") // naming convention for type parameter
 public interface FramePopulator<FRAME,OBJECT>
 {
     /**
      * Populate frame for this rootItem using giving bean name
      *
-     * @param rootItem SwingIntrospectorRootItem to populate
-     * @param beanname bean name to use for identify value from OBJECT
-     * @throws SwingIntrospectorException if any
-     * @throws IntrospectionInvokeException if any
+     * @param rootItem
+     *            SwingIntrospectorRootItem to populate
+     * @param beanname
+     *            bean name to use for identify value from OBJECT
+     * @throws SwingIntrospectorException
+     *             if any
+     * @throws IntrospectionInvokeException
+     *             if any
      */
+    @SuppressWarnings("squid:S1160") // Exception are not of same kind at all.
     void populateFrame(
-            SwingIntrospectorRootItem<FRAME>    rootItem,
-            String                              beanname
-            )
-    throws SwingIntrospectorException,
-           IntrospectionInvokeException;
+        SwingIntrospectorRootItem<FRAME>    rootItem,
+        String                              beanname
+        ) throws SwingIntrospectorException,
+                 IntrospectionInvokeException;
 
 
 }
