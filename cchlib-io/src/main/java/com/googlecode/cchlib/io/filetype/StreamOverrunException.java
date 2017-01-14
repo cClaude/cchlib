a2b4591aba92db5a068c33ef65870826c7ac0c4e
CClaude <Claude.CHOISNET@gmail.com>
2017-01-14 20:14:55 +0100

    Cleaning

--- cchlib-io/src/main/java/com/googlecode/cchlib/io/filetype/StreamOverrunException.java
+++ cchlib-io/src/main/java/com/googlecode/cchlib/io/filetype/StreamOverrunException.java
@@ -2,19 +2,12 @@ package com.googlecode.cchlib.io.filetype;
 
 import java.io.IOException;
 
-/**
- *
- */
 public class StreamOverrunException extends IOException
 {
     private static final long serialVersionUID = 1L;
 
     public StreamOverrunException()
     {
-    }
-
-    public StreamOverrunException( String message )
-    {
-        super( message );
+        // Message not really required
     }
 }
