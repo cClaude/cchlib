d43a1a0175c34deabb2abc6cae725aadcbd2f364
CClaude <Claude.CHOISNET@gmail.com>
2017-01-14 23:06:17 +0100

    Cleaning

--- cchlib-i18n/src/main/java/com/googlecode/cchlib/i18n/core/resolve/MissingKeyException.java
+++ cchlib-i18n/src/main/java/com/googlecode/cchlib/i18n/core/resolve/MissingKeyException.java
@@ -1,18 +1,15 @@
 package com.googlecode.cchlib.i18n.core.resolve;
 
-/**
- *
- */
-public class MissingKeyException extends I18nFieldException 
+public class MissingKeyException extends I18nFieldException
 {
     private static final long serialVersionUID = 1L;
 
-    public MissingKeyException( String message, Throwable cause )
+    public MissingKeyException( final String message, final Throwable cause )
     {
         super( message, cause );
     }
 
-    public MissingKeyException( Throwable cause )
+    public MissingKeyException( final Throwable cause )
     {
         super( cause );
     }
