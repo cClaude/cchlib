503aa2dd5f7816784f8ae81ac6c37aaba0dd2b73
CClaude <Claude.CHOISNET@gmail.com>
2017-01-15 13:39:44 +0100

    Cleaning

--- cchlib-net/src/main/java/com/googlecode/cchlib/net/download/cache/PersistenceFileBadFormatException.java
+++ cchlib-net/src/main/java/com/googlecode/cchlib/net/download/cache/PersistenceFileBadFormatException.java
@@ -5,7 +5,7 @@ import java.io.IOException;
 /**
  * Error when load cache
  */
-public class PersistenceFileBadFormatException // $codepro.audit.disable serializableUsage
+public class PersistenceFileBadFormatException
     extends IOException
 {
     private static final long serialVersionUID = 1L;
