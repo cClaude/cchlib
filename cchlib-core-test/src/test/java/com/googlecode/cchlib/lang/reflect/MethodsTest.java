a1dbf1ffee9fb8d599bb203580e710039328625b
CClaude <Claude.CHOISNET@gmail.com>
2017-01-20 21:17:15 +0100

    Add docs and tests

--- cchlib-core-test/src/test/java/com/googlecode/cchlib/lang/reflect/MethodsTest.java
+++ cchlib-core-test/src/test/java/com/googlecode/cchlib/lang/reflect/MethodsTest.java
@@ -2,6 +2,7 @@ package com.googlecode.cchlib.lang.reflect;
 
 import static org.fest.assertions.api.Assertions.assertThat;
 import java.io.File;
+import java.lang.reflect.AccessibleObject;
 import java.lang.reflect.Method;
 import java.util.Iterator;
 import java.util.Map;
@@ -18,7 +19,7 @@ public class MethodsTest
     {
         final Iterable<Method> methodsOnFile = Methods.getStaticMethods( File.class );
 
-        LOGGER.info( "methodsOnFile" + methodsOnFile );
+        LOGGER.info( "methodsOnFile = " + methodsOnFile );
 
         assertThat( methodsOnFile ).hasSize( 3 );
 
@@ -44,11 +45,11 @@ public class MethodsTest
     public void test_static_on_System()
     {
         final Iterable<Method> methodsOnSystem = Methods.getStaticMethods( System.class );
-        LOGGER.info( "methodsOnSystem" + methodsOnSystem );
+        LOGGER.info( "methodsOnSystem = " + methodsOnSystem );
         assertThat( methodsOnSystem ).hasSize( 27 );
 
         final Map<String,Set<Method>> methodsOnSystemByName = Methods.getStaticMethodsByNames( System.class );
-        LOGGER.info( "methodsOnSystemByName" + methodsOnSystemByName );
+        LOGGER.info( "methodsOnSystemByName = " + methodsOnSystemByName );
 
         assertThat( methodsOnSystemByName ).hasSize( 25 );
         assertThat( methodsOnSystemByName )
@@ -91,5 +92,69 @@ public class MethodsTest
                     break;
             }
         }
+    }
+
+    @Test
+    public void test_static_on_Method()
+    {
+        final Map<String, Set<Method>> methodsOnMethod = Methods.getStaticMethodsByNames( Method.class );
+
+        LOGGER.info( "methodsOnMethod = " + methodsOnMethod );
+
+        assertThat( methodsOnMethod ).hasSize( 1 );
+
+        final Set<Method> setAccessibleSet = methodsOnMethod.get( "setAccessible" );
+        assertThat( setAccessibleSet ).hasSize( 1 );
+
+        final Iterator<Method> iterator = setAccessibleSet.iterator();
+
+        assertThat( iterator.hasNext() ).isTrue();
+        final Method setAccessibleMethod = iterator.next();
+
+        @SuppressWarnings("unchecked")
+        final Class<AccessibleObject> declaringClass = (Class<AccessibleObject>)setAccessibleMethod.getDeclaringClass();
+
+        // this public static method is not on Method
+        assertThat( declaringClass ).isEqualTo( AccessibleObject.class );
+
+        assertThat( iterator.hasNext() ).isFalse();
+    }
+
+    @Test
+    public void test_PublicMethods_on_File()
+    {
+        final Map<String, Set<Method>> publicMethodsOnFile = Methods.getPublicMethodByNames( File.class );
+
+        LOGGER.info( "publicMethodsOnFile = " + publicMethodsOnFile );
+
+        assertThat( publicMethodsOnFile ).hasSize( 47 ); // 57
+
+        int count = 0;
+        for( final Map.Entry<String, Set<Method>> nameMethods : publicMethodsOnFile.entrySet() ) {
+            final String name = nameMethods.getKey();
+
+            switch( name ) {
+                case "compareTo" :
+                case "createTempFile" :
+                case "list" :
+                case "setExecutable" :
+                case "setReadable" :
+                case "setWritable" :
+                    assertThat( nameMethods.getValue() ).hasSize( 2 );
+                    count += 2;
+                    break;
+                case "listFiles" :
+                case "wait" :
+                    assertThat( nameMethods.getValue() ).hasSize( 3 );
+                    count += 3;
+                    break;
+                default:
+                    assertThat( nameMethods.getValue() ).hasSize( 1 );
+                    count += 1;
+                    break;
+            }
+        }
+
+        assertThat( count ).isEqualTo( 57 );
     }
 }
--- cchlib-core/src/main/java/com/googlecode/cchlib/lang/reflect/Methods.java
+++ cchlib-core/src/main/java/com/googlecode/cchlib/lang/reflect/Methods.java
@@ -7,6 +7,7 @@ import java.util.Map;
 import java.util.Set;
 import java.util.TreeMap;
 import java.util.TreeSet;
+import java.util.function.Predicate;
 import com.googlecode.cchlib.NeedDoc;
 import com.googlecode.cchlib.util.CollectionHelper;
 
@@ -19,82 +20,9 @@ public final class Methods
 {
     private Methods(){}
 
-    /**
-     * Returns an {@link Iterable} containing Method objects reflecting all the
-     * public statics methods of the class or interface represented by the Class
-     * object ({@code type}), including those declared by the class or interface
-     * and those inherited from superclasses and superinterfaces.
-     *
-     * @param type The class object
-     * @return all static methods of the giving class, {@link Method} are by name
-     */
-    public static Iterable<Method> getStaticMethods( final Class<?> type )
+    private static int compare( final Class<?> class1, final Class<?> class2 )
     {
-        @SuppressWarnings("squid:S2293") // SonarError: could diamond operator here
-        final Set<Method> methods = new TreeSet<Method>( Methods::compare );
-
-        for( final Method method : type.getMethods() ) {
-            if( Modifier.isStatic( method.getModifiers() ) ) {
-                methods.add( method );
-                }
-            }
-
-        return Collections.unmodifiableCollection( methods );
-    }
-
-    @SuppressWarnings("squid:S2293") // SonarError: could diamond operator here
-    public static Map<String, Set<Method>> getStaticMethodsByNames( final Class<?> type )
-    {
-        final Map<String, Set<Method>> methods = new TreeMap<String, Set<Method>>( Methods::compare );
-        final Iterable<Method> staticMethods   = getStaticMethods( type );
-
-        for( final Method method : staticMethods ) {
-            Set<Method> set = methods.get( method.getName() );
-
-            if( set == null ) {
-                set = new TreeSet<Method>( Methods::compare );
-                methods.put( method.getName(), set );
-            }
-
-            set.add( method );
-        }
-
-        return CollectionHelper.unmodifiableMapSet( methods );
-    }
-
-    public static Iterable<Method> getPublicMethods( final Class<?> clazz )
-    {
-        @SuppressWarnings("squid:S2293") // SonarError: could diamond operator here
-        final Set<Method> methods = new TreeSet<Method>( Methods::compare );
-
-        for( final Method method : clazz.getMethods() ) {
-
-            if( !Modifier.isPublic( method.getModifiers() ) ) {
-                // Should be always true by desing
-                throw new IllegalStateException();
-                }
-
-            methods.add( method );
-            }
-
-        return Collections.unmodifiableCollection( methods );
-    }
-
-    private static int compare( final Method m1, final Method m2 )
-    {
-        final String n1 = m1.getName();
-        final String n2 = m2.getName();
-
-        if( n1.equals( n2 ) ) {
-            return compare( m1.getParameterTypes(), m2.getParameterTypes() );
-        } else {
-            return n1.compareTo( n2 );
-        }
-    }
-
-    private static int compare( final String n1, final String n2 )
-    {
-        return n1.compareTo( n2 );
+        return class1.getName().compareTo( class2.getName() );
     }
 
     @SuppressWarnings("squid:S134") // too deeply
@@ -119,8 +47,158 @@ public final class Methods
         }
     }
 
-    private static int compare( final Class<?> class1, final Class<?> class2 )
+    private static int compare( final Method m1, final Method m2 )
     {
-        return class1.getName().compareTo( class2.getName() );
+        final String n1 = m1.getName();
+        final String n2 = m2.getName();
+
+        if( n1.equals( n2 ) ) {
+            return compare( m1.getParameterTypes(), m2.getParameterTypes() );
+        } else {
+            return n1.compareTo( n2 );
+        }
+    }
+
+    private static int compare( final String n1, final String n2 )
+    {
+        return n1.compareTo( n2 );
+    }
+
+    /**
+     * Returns an {@link Map} containing Method objects reflecting all the
+     * public statics methods of the class or interface represented by the Class
+     * object ({@code type}), including those declared by the class or interface
+     * and those inherited from superclasses and superinterfaces.
+     * <ul>
+     * <li>Keys map are method names</li>
+     * <li>Values map are {@link Set} of {@link Method}
+     * </li>
+     *
+     * @see Class#getMethods()
+     * @since 4.2
+     */
+    public static Map<String, Set<Method>> getPublicMethodByNames( final Class<?> type )
+    {
+        return CollectionHelper.unmodifiableMapSet( toMapSet( getPublicMethodSet( type ) ) );
+    }
+
+    /**
+     * Returns an {@link Iterable} containing Method objects reflecting all the
+     * public statics methods of the class or interface represented by the Class
+     * object ({@code type}), including those declared by the class or interface
+     * and those inherited from superclasses and superinterfaces.
+     *
+     * @param type The class object
+     * @return all static methods of the giving class,
+     *         {@link Method} sorted are by ( name, then parameters types )
+     *
+     * @param type The class object
+     * @return all static methods of the giving class,
+     *         {@link Method} sorted are by ( name, then parameters types )
+     *
+     * @see Class#getMethods()
+     */
+    public static Iterable<Method> getPublicMethods( final Class<?> clazz )
+    {
+        return Collections.unmodifiableCollection( getPublicMethodSet( clazz ) );
+    }
+
+    private static Set<Method> getPublicMethodSet( final Class<?> clazz )
+    {
+        @SuppressWarnings("squid:S2293") // SonarError: could diamond operator here
+        final Set<Method> methods = new TreeSet<Method>( Methods::compare );
+
+        for( final Method method : clazz.getMethods() ) {
+            methods.add( method );
+            }
+
+        return methods;
+    }
+
+    /**
+     * Returns an {@link Iterable} containing static Method objects reflecting all the
+     * public statics methods of the class or interface represented by the Class
+     * object ({@code type}), including those declared by the class or interface
+     * and those inherited from superclasses and superinterfaces.
+     *
+     * @param type The class object
+     * @return all static methods of the giving class,
+     *         {@link Method} sorted are by ( name, then parameters types )
+     *
+     * @see Class#getMethods()
+     */
+    public static Iterable<Method> getStaticMethods( final Class<?> type )
+    {
+        return Collections.unmodifiableCollection(
+                getStaticMethodSet( type, getStaticMethods() )
+                );
+    }
+
+    private static Predicate<Method> getStaticMethods()
+    {
+        return method -> Modifier.isStatic( method.getModifiers() );
+    }
+
+    /**
+     * Returns an {@link Map} containing static Method objects reflecting all the
+     * public statics methods of the class or interface represented by the Class
+     * object ({@code type}), including those declared by the class or interface
+     * and those inherited from superclasses and superinterfaces.
+     * <ul>
+     * <li>Keys map are method names</li>
+     * <li>Values map are {@link Set} of {@link Method}
+     * </li>
+     *
+     * @param type The class object
+     * @return all static methods of the giving class,
+     *         {@link Method} sorted are by ( name, then parameters types )
+     *
+     * @see Class#getMethods()
+     * @since 4.2
+     */
+    public static Map<String, Set<Method>> getStaticMethodsByNames( final Class<?> type )
+    {
+        return CollectionHelper.unmodifiableMapSet(
+                toMapSet(
+                    getStaticMethodSet( type, getStaticMethods() )
+                    )
+                );
+    }
+
+    private static Set<Method> getStaticMethodSet(
+            final Class<?>          type,
+            final Predicate<Method> predicate
+            )
+    {
+        @SuppressWarnings("squid:S2293") // SonarError: could diamond operator here
+        final Set<Method> methods = new TreeSet<Method>( Methods::compare );
+
+        for( final Method method : type.getMethods() ) {
+            if( predicate.test( method ) ) {
+                methods.add( method );
+            }
+        }
+
+        return methods;
+    }
+
+    @SuppressWarnings("squid:S2293") // SonarError: could diamond operator here
+    private static Map<String, Set<Method>> toMapSet( final Iterable<Method> methods )
+    {
+        final Map<String, Set<Method>> methodMapSet
+            = new TreeMap<String, Set<Method>>( Methods::compare );
+
+        for( final Method method : methods ) {
+            Set<Method> set = methodMapSet.get( method.getName() );
+
+            if( set == null ) {
+                set = new TreeSet<Method>( Methods::compare );
+                methodMapSet.put( method.getName(), set );
+            }
+
+            set.add( method );
+        }
+
+        return methodMapSet;
     }
 }
