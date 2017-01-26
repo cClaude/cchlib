d9052036523a72eb1147b3ad2438d7faad08cb67
CClaude <Claude.CHOISNET@gmail.com>
2017-01-26 20:48:32 +0100

    Add reverseMap functions

--- cchlib-core/src/main/java/com/googlecode/cchlib/util/CollectionHelper.java
+++ cchlib-core/src/main/java/com/googlecode/cchlib/util/CollectionHelper.java
@@ -4,10 +4,13 @@ import java.util.ArrayList;
 import java.util.Collection;
 import java.util.Collections;
 import java.util.Enumeration;
+import java.util.HashMap;
+import java.util.HashSet;
 import java.util.Iterator;
 import java.util.List;
 import java.util.Map;
 import java.util.Set;
+import java.util.function.Supplier;
 import javax.annotation.Nonnull;
 import javax.annotation.Nullable;
 import com.googlecode.cchlib.NeedTestCases;
@@ -82,8 +85,10 @@ public final class CollectionHelper
      * @param <V>
      *            the class of the set values
      * @param mapSet
-     *            the {@link Map} of {@link Set} for which an unmodifiable view is to be returned
+     *            the {@link Map} of {@link Set} for which an unmodifiable view
+     *            is to be returned
      * @return an unmodifiable view of the specified map.
+     *
      * @see Collections#unmodifiableMap(Map)
      * @see Collections#unmodifiableSet(Set)
      * @since 4.2
@@ -164,8 +169,82 @@ public final class CollectionHelper
      * @return true if collection is empty or null
      * @since 4.2
      */
-    public static <T> boolean isEmpty( @Nullable  final Collection<T> collection )
+    public static <T> boolean isEmpty( @Nullable final Collection<T> collection )
     {
         return (collection == null) || collection.isEmpty();
+    }
+
+    /**
+     * Returns a Map&lt;V,Set&lt;K&gt;&gt;
+     * <p>
+     * {@link Supplier} are based on {@link HashMap} and {@link HashSet}
+     * <p>
+     * Original map is not modify by this operation.
+     *
+     * @param <K>
+     *            The type of the key of the initial map, the type of the value
+     *            in result {@link Map}{@link Set}
+     * @param <V>
+     *            The type of the value of the initial map, the type of the key
+     *            in result {@link Map}{@link Set}
+     * @param map
+     *            The map
+     * @return a {@link Map} build using {@link HashMap} {@link HashSet}
+     *
+     * @see #reverseMap(Map, Supplier, Supplier)
+     */
+    public static <K,V> Map<V,Set<K>> reverseMap( final Map<K,V> map )
+    {
+        final Supplier<Map<V, Set<K>>> mapSupplier = HashMap::new;
+        final Supplier<Set<K>>         setSupplier = HashSet::new;
+
+        return reverseMap( map, mapSupplier, setSupplier );
+    }
+
+    /**
+     * Returns a {@link Map}&lt;V,{@link Set}&lt;K&gt;&gt; build by exchange
+     * key and value of original {@code map}. All couple (K,V) are kept by
+     * this operation.
+     * <p>
+     * Original map is not modify by this operation.
+     *
+     * @param <K>
+     *            The type of the key of the initial map, the type of the value
+     *            in result {@link Map}{@link Set}
+     * @param <V>
+     *            The type of the value of the initial map, the type of the key
+     *            in result {@link Map}{@link Set}
+     * @param map
+     *            The map
+     * @param mapSupplier
+     *            The {@link Map} {@link Supplier}
+     * @param setSupplier
+     *            The {@link Set} {@link Supplier}
+     * @return a {@link Map} build using {@link HashMap} {@link Set}
+     */
+    public static <K,V> Map<V,Set<K>> reverseMap(
+        @Nonnull final Map<K,V>                map,
+        @Nonnull final Supplier<Map<V,Set<K>>> mapSupplier,
+        @Nonnull final Supplier<Set<K>>        setSupplier
+        )
+    {
+        final Map<V,Set<K>> reverseMap = mapSupplier.get();
+
+        for( final Map.Entry<K, V> entry : map.entrySet() ) {
+            final K      oldKey   = entry.getKey();
+            final V      oldValue = entry.getValue();
+            final Set<K> set;
+
+            if( reverseMap.containsKey( oldValue ) ) {
+                set = reverseMap.get( oldValue );
+            } else {
+                set = setSupplier.get();
+
+                reverseMap.put( oldValue, set );
+            }
+            set.add( oldKey );
+        }
+
+        return reverseMap;
     }
 }
